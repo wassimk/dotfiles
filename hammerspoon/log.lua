01de1ea3244c762178178a3ef529d805906db59c
Wassim Metallaoui <wassim@metallaoui.com>
2017-06-30 15:48:24 -0500

    Switch keyboard profiles automatically

---
+++ hammerspoon/log.lua
@@ -0,0 +1,11 @@
+--
+-- Simple logging facility.
+--
+
+local logLevel = 'info' -- generally want 'debug' or 'info'
+local log = hs.logger.new('wassimk', logLevel)
+
+return {
+  i = log.i,
+  w = log.w,
+}
