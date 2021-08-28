f5e8b81aeb5ad03c879de36323443101555a8d95
Wassim Metallaoui <wassim@metallaoui.com>
2021-08-27 20:25:55 -0500

    fix issue prevent gh install

--- install.sh
+++ install.sh
@@ -75,7 +75,7 @@ installHub() {
 installGh() {
   case $os in
     $ubuntu*)
-      if command -v gh 2>&1 >/dev/null; then
+      if ! command -v gh 2>&1 >/dev/null; then
         curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
         echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
         updateAvailablePackages
