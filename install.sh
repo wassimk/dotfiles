cddc0f7c360813658ca994de8722bc97f59bc347
Wassim Metallaoui <wassim@metallaoui.com>
2021-08-28 13:14:43 -0500

    run omz IF it exists

--- install.sh
+++ install.sh
@@ -57,7 +57,7 @@ installZsh() {
 installOMZsh() {
   if [ ! -d "$HOME"/.oh-my-zsh ]; then
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --skip-chsh --keep-zshrc
-  elif ! command -v omz >/dev/null 2>&1; then
+  elif command -v omz >/dev/null 2>&1; then
     omz update
   fi
 }
