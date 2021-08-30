ab40b72ee842b70eab86f732021ed0a18e1512d5
Wassim Metallaoui <wassim@metallaoui.com>
2021-08-30 08:47:01 -0500

    symlink for nevom no longer needed

--- install.sh
+++ install.sh
@@ -313,8 +313,6 @@ installItermShellIntegration() {
 
 setupNeovim() {
   mkdir -p "$HOME"/.config/nvim/
-  ln -sf "$HOME"/.vimrc "$HOME"/.config/nvim/init.vim
-
   if [ ! -f "$HOME"/.local/share/nvim/site/autoload/plug.vim ]; then
     sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
