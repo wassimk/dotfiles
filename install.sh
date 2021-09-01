067cab21ab9ead4c961931fa171c47c96eaae15c
Wassim Metallaoui <wassim@metallaoui.com>
2021-09-01 09:33:04 -0500

    Install gh compleitions on linux

--- install.sh
+++ install.sh
@@ -156,16 +156,21 @@ installHub() {
 
 installGh() {
   case $os in
+    $macOS*)
+      installOrUpdate "gh"
+      ;;
     $ubuntu*)
       if ! command -v gh >/dev/null 2>&1; then
         curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
         echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
         updateAvailablePackages
       fi
+
+      installOrUpdate "gh"
+
+      gh completion -s zsh | sudo tee -a /usr/local/share/zsh/site-functions/_gh > /dev/null
       ;;
   esac
-
-  installOrUpdate "gh"
 }
 
 installHeroku() {
