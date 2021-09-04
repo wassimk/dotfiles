fa862798fe29fdc7cf7a27b9190ad8f5cb08c950
Wassim Metallaoui <wassim@metallaoui.com>
2021-09-03 23:59:59 -0500

    fix autopairs plugin conflict

--- config/nvim/lua/completions.lua
+++ config/nvim/lua/completions.lua
@@ -58,6 +58,7 @@ cmp.setup {
 }
 
 -- Autopairs
+require("nvim-autopairs").setup({})
 require("nvim-autopairs.completion.cmp").setup(
   { map_cr = true, map_complete = true, auto_select = true }
 )
