659e34c6ffa5ca2f7253b4605c6d5b372591a192
Wassim Metallaoui <wassim@metallaoui.com>
2022-05-11 10:21:21 -0500

    feat: add autocmd for hiding non-focused cursor and closing windows

--- config/nvim/lua/wassim/autocmds.lua
+++ config/nvim/lua/wassim/autocmds.lua
@@ -1,21 +1,21 @@
 local api = vim.api
 local wamGrp = api.nvim_create_augroup('WamAutocmds', { clear = true })
 
--- Highlight on yank
+-- highlight on yank
 api.nvim_create_autocmd('TextYankPost', {
   pattern = '*',
   command = 'silent! lua vim.highlight.on_yank { timeout = 750 }',
   group = wamGrp,
 })
 
--- Wrap long text in quickfix windows
+-- wrap long text in quickfix windows
 api.nvim_create_autocmd('FileType', {
   pattern = 'qf',
   command = 'setlocal wrap',
   group = wamGrp,
 })
 
--- Center current window when is resized
+-- center current window when is resized
 -- FIXME: this doesn't seem to work, you can manally do it and it behaves different
 api.nvim_create_autocmd('VimResized', {
   pattern = '*',
@@ -36,3 +36,20 @@ api.nvim_create_autocmd('FileType', {
   command = 'setlocal spell',
   group = wamGrp,
 })
+
+-- show cursor line only in active window
+api.nvim_create_autocmd(
+  { 'InsertLeave', 'WinEnter' },
+  { pattern = '*', command = 'set cursorline', group = wamGrp }
+)
+api.nvim_create_autocmd(
+  { 'InsertEnter', 'WinLeave' },
+  { pattern = '*', command = 'set nocursorline', group = wamGrp }
+)
+
+-- close certain windows to close with "q"
+api.nvim_create_autocmd(
+  'FileType',
+  { pattern = { 'help', 'startuptime', 'qf', 'lspinfo' }, command = [[nnoremap <buffer><silent> q :close<CR>]] }
+)
+api.nvim_create_autocmd('FileType', { pattern = 'man', command = [[nnoremap <buffer><silent> q :quit<CR>]] })
