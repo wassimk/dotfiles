e7bb722c2fc93397dcc8a7b6ce67ade34945dc20
Wassim Metallaoui <wassim@metallaoui.com>
2022-05-11 09:13:58 -0500

    chore: move autocmds over to lua

--- config/nvim/lua/wassim/autocmds.lua
+++ config/nvim/lua/wassim/autocmds.lua
@@ -1,8 +1,38 @@
 local api = vim.api
+local wamGrp = api.nvim_create_augroup('WamAutocmds', { clear = true })
 
 -- Highlight on yank
-local yankGrp = api.nvim_create_augroup('YankHighlight', { clear = true })
 api.nvim_create_autocmd('TextYankPost', {
+  pattern = '*',
   command = 'silent! lua vim.highlight.on_yank { timeout = 750 }',
-  group = yankGrp,
+  group = wamGrp,
+})
+
+-- Wrap long text in quickfix windows
+api.nvim_create_autocmd('FileType', {
+  pattern = 'qf',
+  command = 'setlocal wrap',
+  group = wamGrp,
+})
+
+-- Center current window when is resized
+-- FIXME: this doesn't seem to work, you can manally do it and it behaves different
+api.nvim_create_autocmd('VimResized', {
+  pattern = '*',
+  command = 'execute "normal! \\<c-w>="',
+  group = wamGrp,
+})
+
+-- markdown
+api.nvim_create_autocmd('FileType', {
+  pattern = 'markdown',
+  command = 'setlocal spell | setlocal textwidth=100',
+  group = wamGrp,
+})
+
+-- gitcommit
+api.nvim_create_autocmd('FileType', {
+  pattern = 'gitcommit',
+  command = 'setlocal spell',
+  group = wamGrp,
 })
