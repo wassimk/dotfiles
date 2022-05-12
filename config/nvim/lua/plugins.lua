c9a3f277b9bd3a94c3923d0350ea282e91451fcb
Wassim Metallaoui <wassim@metallaoui.com>
2022-05-12 15:26:54 -0500

    feat: add lsp signature helper

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -32,6 +32,7 @@ return require('packer').startup(function(use)
     requires = {
       'hrsh7th/cmp-buffer',
       'hrsh7th/cmp-cmdline',
+      'hrsh7th/cmp-nvim-lsp-signature-help',
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-nvim-lua',
       'hrsh7th/cmp-path',
