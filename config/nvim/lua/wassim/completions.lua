67a431519219c2e1091f6ffbaa62b907eadd1a0e
Wassim Metallaoui <wassim@metallaoui.com>
2022-05-12 11:47:39 -0500

    feat: use new cmp api

--- config/nvim/lua/wassim/completions.lua
+++ config/nvim/lua/wassim/completions.lua
@@ -28,8 +28,8 @@ cmp.setup {
     ['<C-u>'] = cmp.mapping.scroll_docs(-4),
     ['<C-d>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.close(),
-    ['<CR>'] = cmp.mapping.confirm { behavior = cmp.ConfirmBehavior.Insert, select = true },
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm { select = true },
   },
   sources = {
     { name = 'buffer', dup = 0 }, { name = 'nvim_lsp' }, { name = 'snippy' }, { name = 'nvim_lua' },
