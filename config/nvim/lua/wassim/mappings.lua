c545f7784f1db887596fa5b93fa8c7a18b568510
Wassim Metallaoui <wassim@metallaoui.com>
2022-05-16 15:06:35 -0500

    fix: change Telescope mappings to not interfere with jumping forward

--- config/nvim/lua/wassim/mappings.lua
+++ config/nvim/lua/wassim/mappings.lua
@@ -22,8 +22,8 @@ vim.api.nvim_set_keymap('n', 't<C-l>', ':TestLast<CR>', { silent = true })
 vim.api.nvim_set_keymap('n', 't<C-g>', ':TestVisit<CR>', { silent = true })
 
 -- telescope mappings
-vim.api.nvim_set_keymap('n', 'ff', ':Telescope find_files<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', 'fg', ':Telescope live_grep<CR>', { noremap = tru, silent = true })
-vim.api.nvim_set_keymap('n', 'fb', ':Telescope current_buffer_fuzzy_find<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', 'fob', ':Telescope buffers<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', 'fh', ':Telescope help_tags<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-f>f', ':Telescope find_files<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-f>g', ':Telescope live_grep<CR>', { noremap = tru, silent = true })
+vim.api.nvim_set_keymap('n', '<C-f>b', ':Telescope current_buffer_fuzzy_find<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-f>ob', ':Telescope buffers<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-f>h', ':Telescope help_tags<CR>', { noremap = true, silent = true })
