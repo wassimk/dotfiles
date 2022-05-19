bbb47a647b119540deae41fae1ab1e06727c4c4f
Wassim Metallaoui <wassim@metallaoui.com>
2022-05-19 10:28:12 -0500

    change: use new vim.keymap.set option
    Unless the mapping is <plug> then it will noremap by default. And now we
    can pass a lua function reference instead of a lua string command.
    
    buffer = 0 only maps for the current buffer.

--- config/nvim/lua/wassim/mappings.lua
+++ config/nvim/lua/wassim/mappings.lua
@@ -5,29 +5,31 @@
 -- custom leader mapping
 vim.cmd('let mapleader = "\\<Space>"')
 
+local opts = { silent = true }
+
 -- nvim-tree toggle
-vim.api.nvim_set_keymap('n', '<C-n>', '<cmd>NvimTreeToggle<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-n>', '<cmd>NvimTreeToggle<CR>', opts)
 
--- Edit configuration files
-vim.api.nvim_set_keymap('n', '<Leader>ev', '<cmd>vsplit $MYVIMRC<CR>', { silent = true })
-vim.api.nvim_set_keymap('n', '<Leader>ez', '<cmd>vsplit ~/.zshrc<CR>', { silent = true })
-vim.api.nvim_set_keymap('n', '<Leader>et', '<cmd>vsplit ~/.tmux.conf<CR>', { silent = true })
-vim.api.nvim_set_keymap('n', '<Leader>sv', '<cmd>source $MYVIMRC<CR>', { silent = true })
+-- edit configuration files
+vim.keymap.set('n', '<leader>ev', '<cmd>vsplit $MYVIMRC<CR>', opts)
+vim.keymap.set('n', '<leader>ez', '<cmd>vsplit ~/.zshrc<CR>', opts)
+vim.keymap.set('n', '<leader>et', '<cmd>vsplit ~/.tmux.conf<CR>', opts)
+vim.keymap.set('n', '<leader>sv', '<cmd>source $MYVIMRC<CR>', opts)
 
--- vim-test mappings
-vim.api.nvim_set_keymap('n', 't<C-n>', '<cmd>TestNearest<CR>', { silent = true })
-vim.api.nvim_set_keymap('n', 't<C-f>', '<cmd>TestFile<CR>', { silent = true })
-vim.api.nvim_set_keymap('n', 't<C-a>', '<cmd>TestSuite<CR>', { silent = true })
-vim.api.nvim_set_keymap('n', 't<C-l>', '<cmd>TestLast<CR>', { silent = true })
-vim.api.nvim_set_keymap('n', 't<C-g>', '<cmd>TestVisit<CR>', { silent = true })
+-- vim-test
+vim.keymap.set('n', 't<C-n>', '<cmd>TestNearest<CR>', opts)
+vim.keymap.set('n', 't<C-f>', '<cmd>TestFile<CR>', opts)
+vim.keymap.set('n', 't<C-a>', '<cmd>TestSuite<CR>', opts)
+vim.keymap.set('n', 't<C-l>', '<cmd>TestLast<CR>', opts)
+vim.keymap.set('n', 't<C-g>', '<cmd>TestVisit<CR>', opts)
 
--- telescope mappings
-vim.api.nvim_set_keymap('n', '<C-f>f', '<cmd>Telescope find_files<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-f>g', '<cmd>Telescope live_grep<CR>', { noremap = tru, silent = true })
-vim.api.nvim_set_keymap('n', '<C-f>b', '<cmd>Telescope current_buffer_fuzzy_find<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-f>ob', '<cmd>Telescope buffers<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-f>h', '<cmd>Telescope help_tags<CR>', { noremap = true, silent = true })
+-- telescope
+vim.keymap.set('n', '<C-f>f', '<cmd>Telescope find_files<CR>', opts)
+vim.keymap.set('n', '<C-f>g', '<cmd>Telescope live_grep<CR>', opts)
+vim.keymap.set('n', '<C-f>b', '<cmd>Telescope current_buffer_fuzzy_find<CR>', opts)
+vim.keymap.set('n', '<C-f>ob', '<cmd>Telescope buffers<CR>', opts)
+vim.keymap.set('n', '<C-f>h', '<cmd>Telescope help_tags<CR>', opts)
 
 -- bufferline
-vim.api.nvim_set_keymap('n', ']b', '<cmd>BufferLineCycleNext<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '[b', '<cmd>BufferLineCyclePrev<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', ']b', '<cmd>BufferLineCycleNext<CR>', opts)
+vim.keymap.set('n', '[b', '<cmd>BufferLineCyclePrev<CR>', opts)
