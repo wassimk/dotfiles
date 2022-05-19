9cb1e2ac80b06198f149bb80336963b26ff05289
Wassim Metallaoui <wassim@metallaoui.com>
2022-05-19 09:08:35 -0500

    feat: tweak the new lualine configuration to match vim-airline

--- config/nvim/lua/wassim/statusline.lua
+++ config/nvim/lua/wassim/statusline.lua
@@ -1,20 +1,27 @@
+local mode_map = {
+  ['c']  = 'C',
+  ['i']  = 'I',
+  ['ic'] = 'IC',
+  ['n']  = 'N',
+  ['t']  = 'T',
+  ['v']  = 'V',
+  ['V']  = 'VL'
+}
+
 require('lualine').setup {
   options = {
-    icons_enabled = true,
-    theme = 'auto',
-    component_separators = { left = '', right = '' },
-    section_separators = { left = '', right = '' },
     disabled_filetypes = {},
-    always_divide_middle = true,
     globalstatus = false,
   },
   sections = {
-    lualine_a = { 'mode' },
-    lualine_b = { 'branch', 'diff', 'diagnostics' },
+    lualine_a = { function()
+      return mode_map[vim.api.nvim_get_mode().mode] or '__'
+    end },
+    lualine_b = { 'branch', 'diff' },
     lualine_c = { 'filename' },
-    lualine_x = { 'encoding', 'fileformat', 'filetype' },
-    lualine_y = { 'progress' },
-    lualine_z = { 'location' }
+    lualine_x = { 'filetype' },
+    lualine_y = { 'diagnostics' },
+    lualine_z = { "'ℓ %l 𝚌 %v'" }, -- ℓ symbol breaks without the inner quotes
   },
   inactive_sections = {
     lualine_a = {},
@@ -24,6 +31,5 @@ require('lualine').setup {
     lualine_y = {},
     lualine_z = {}
   },
-  tabline = {},
-  extensions = {}
+  extensions = { 'quickfix', 'nvim-tree', 'fugitive' }
 }
