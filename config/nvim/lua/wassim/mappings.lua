93439925538f37e4ce37a0b362ab3477fbb0c9a9
Wassim Metallaoui <wassim@metallaoui.com>
2022-05-19 18:56:28 -0500

    feat: add mapping to quickly get a searchable command list

--- config/nvim/lua/wassim/mappings.lua
+++ config/nvim/lua/wassim/mappings.lua
@@ -29,6 +29,7 @@ vim.keymap.set('n', '<C-f>g', '<cmd>Telescope live_grep<CR>', opts)
 vim.keymap.set('n', '<C-f>b', '<cmd>Telescope current_buffer_fuzzy_find<CR>', opts)
 vim.keymap.set('n', '<C-f>ob', '<cmd>Telescope buffers<CR>', opts)
 vim.keymap.set('n', '<C-f>h', '<cmd>Telescope help_tags<CR>', opts)
+vim.keymap.set('n', '<C-f>c', '<cmd>Telescope commands<CR>', opts)
 
 -- bufferline
 vim.keymap.set('n', ']b', '<cmd>BufferLineCycleNext<CR>', opts)
