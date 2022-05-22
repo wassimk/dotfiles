1c66f93b1422146bcb04c5d2b723623cdeaa2357
Wassim Metallaoui <wassim@metallaoui.com>
2022-05-22 17:41:05 -0500

    change: don't show title in tabline on nvimtree

--- config/nvim/lua/wassim/tabline.lua
+++ config/nvim/lua/wassim/tabline.lua
@@ -9,11 +9,7 @@ require('bufferline').setup {
     offsets = {
       {
         filetype = 'NvimTree',
-        text = function()
-          return vim.fn.getcwd()
-        end,
-        highlight = 'Directory',
-        text_align = 'left'
+        text = nil,
       }
     }
   }
