3c4545c96877320ea18c21365a48119be00c8474
Wassim Metallaoui <wassim@metallaoui.com>
2022-05-22 08:58:23 -0500

    fix: remove unnecessary runtime path for sumneko_lua
    References:
    https://github.com/neovim/nvim-lspconfig/pull/1915

--- config/nvim/lua/wassim/lsp.lua
+++ config/nvim/lua/wassim/lsp.lua
@@ -98,14 +98,9 @@ require 'lspconfig'.eslint.setup {
 }
 
 -- lua
-local runtime_path = vim.split(package.path, ';')
-table.insert(runtime_path, 'lua/?.lua')
-table.insert(runtime_path, 'lua/?/init.lua')
-
 require('lspconfig').sumneko_lua.setup {
   settings = {
     Lua = {
-      runtime = { path = runtime_path },
       diagnostics = { globals = { 'vim', 'hs' } },
       workspace = { library = vim.api.nvim_get_runtime_file('', true) },
       format = { enable = true },
