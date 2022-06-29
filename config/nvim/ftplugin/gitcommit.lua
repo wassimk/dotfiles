6e26a7d67b13302333aeb97d2eb995d39d3925ad
Wassim Metallaoui <wassim@metallaoui.com>
2022-06-29 10:25:40 -0500

    chore: wrap require with parens

--- config/nvim/ftplugin/gitcommit.lua
+++ config/nvim/ftplugin/gitcommit.lua
@@ -1,6 +1,6 @@
 vim.o.spell = true
 
-require 'wassim.cmp.handles'.setup()
+require('wassim.cmp.handles').setup()
 
 -- TODO: move the git handle out of wassim.cmp.handles and back into cmp/init.lua
 require('cmp_git').setup()
