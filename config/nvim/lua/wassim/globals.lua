eec6680356ab4fe7a397ed7be7e8fa095bab0082
Wassim Metallaoui <wassim@metallaoui.com>
2022-06-29 20:37:50 -0500

    feat: add a global logger in lua
    Just call LOG('anything') and it writes to ~/.cache/nvim/globals-log.log

--- config/nvim/lua/wassim/globals.lua
+++ config/nvim/lua/wassim/globals.lua
@@ -1,10 +1,15 @@
 P = function(v)
-  print (vim.inspect(v))
+  print(vim.inspect(v))
   return v
 end
 
+LOG = function(...)
+  logger = require('plenary.log').new { plugin = 'globals-log' }
+  return logger.info(...)
+end
+
 RELOAD = function(...)
-  return require("plenary.reload").reload_module(...)
+  return require('plenary.reload').reload_module(...)
 end
 
 R = function(name)
