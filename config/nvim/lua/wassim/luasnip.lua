d0d7cb5537d88ef36b73378738d29ef172a1b680
Wassim Metallaoui <wassim@metallaoui.com>
2022-07-05 19:43:21 -0500

    feat: remember last snippet and support dynamic text

--- config/nvim/lua/wassim/luasnip.lua
+++ config/nvim/lua/wassim/luasnip.lua
@@ -4,6 +4,8 @@ if has_luasnip then
   local types = require('luasnip.util.types')
 
   luasnip.config.setup {
+    history = true,
+    update_events = 'InsertLeave,TextChangedI',
     ext_opts = {
       [types.choiceNode] = {
         active = {
@@ -17,7 +19,7 @@ if has_luasnip then
       -- },
     },
     -- store_selection_keys = '<Tab>',
-    -- update_events = 'InsertLeave,TextChangedI',
+    -- enable_autosnippets = true,
   }
 
   require('luasnip.loaders.from_lua').load { paths = '~/.config/nvim/snippets' }
