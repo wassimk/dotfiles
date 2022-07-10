4056bcb971b6f523f4f1119102b0f30e5bff0ebb
Wassim Metallaoui <wassim@metallaoui.com>
2022-07-10 17:25:24 -0500

    feat: add use_stripe_cassette snippets, simple and object
    They are hidden from cmp completion by default because they also show
    up outside of tests/ folder. I tried to use a function to set hidden to
    false when inside of a test folder but it doesn't work. I think it's
    because the code is compiled to the value when it's loaded. I think it
    needs to take a function work right.
    
    To expand it in insert mode:
    use_stripe_cassette<C-k>

--- config/nvim/snippets/ruby.lua
+++ config/nvim/snippets/ruby.lua
@@ -7,12 +7,43 @@ if not has_luasnip then
 end
 
 return {
-  s({
-    trig = 'rl',
-    namr = 'Rails Logger',
-    dscr = 'Write to the built-in Rails logger'
-  },
-
-    { t { "Rails.logger.info '#' * 50", 'Rails.logger.info ', }, i(0, 'data_to_log'), t { '', "Rails.logger.info '#' * 50" } }
+  s(
+    {
+      trig = 'rl',
+      name = 'Rails Logger',
+      dscr = 'Write to the built-in Rails logger'
+    },
+    {
+      t { "Rails.logger.info '#' * 50", 'Rails.logger.info ', },
+      i(0, 'data_to_log'),
+      t { '', "Rails.logger.info '#' * 50" }
+    }
+  ),
+  s(
+    {
+      trig = 'use_stripe_cassette',
+      name = 'Stripe VCR Cassette (object)',
+      dscr = 'Stripe VCR cassette with object from cassette data',
+      hidden = true,
+    },
+    {
+      t { 'use_stripe_cassette("' }, i(1, 'cassette-name'), t { '") do |cassette|', '' },
+      t { '  ' }, i(2, 'stripe_object-name'), t { ' = stripe_object_from_cassette(cassette, path: "' }, i(3, 'request-path'), t { '", http_method: :' }, c(4, { t 'post', t 'get', t 'put', t 'delete' }), t { ')', '' },
+      t { '  ' }, i(0),
+      t { '', 'end' }
+    }
+  ),
+  s(
+    {
+      trig = 'use_stripe_cassette',
+      name = 'Stripe VCR Cassette (simple)',
+      dscr = 'Stripe VCR cassette',
+      hidden = true,
+    },
+    {
+      t { 'use_stripe_cassette("' }, i(1), t { '") do', '' },
+      t { '  ' }, i(0),
+      t { '', 'end' }
+    }
   )
 }
