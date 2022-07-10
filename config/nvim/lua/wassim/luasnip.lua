6ea5c8c8b850ebeef4f2836ad3072fff6689d6bf
Wassim Metallaoui <wassim@metallaoui.com>
2022-07-10 17:24:27 -0500

    feat: j/k jump in snippets and snippet ui tweaking

--- config/nvim/lua/wassim/luasnip.lua
+++ config/nvim/lua/wassim/luasnip.lua
@@ -5,11 +5,11 @@ if has_luasnip then
 
   luasnip.config.setup {
     history = true,
-    update_events = 'InsertLeave,TextChangedI',
+    updateevents = 'TextChanged,TextChangedI',
     ext_opts = {
       [types.choiceNode] = {
         active = {
-          virt_text = { { '← Choice' } },
+          virt_text = { { '←  Choice', 'Comment' } },
         },
       },
       -- [types.insertNode] = {
@@ -21,6 +21,18 @@ if has_luasnip then
     -- store_selection_keys = '<Tab>',
     -- enable_autosnippets = true,
   }
+
+  vim.keymap.set({ 'i', 's' }, '<C-k>', function()
+    if luasnip.expand_or_jumpable() then
+      luasnip.expand_or_jump()
+    end
+  end, { silent = true })
+
+  vim.keymap.set({ 'i', 's' }, '<C-j>', function()
+    if luasnip.jumpable(-1) then
+      luasnip.jump(-1)
+    end
+  end, { silent = true })
 
   require('luasnip.loaders.from_lua').load { paths = '~/.config/nvim/snippets' }
 end
