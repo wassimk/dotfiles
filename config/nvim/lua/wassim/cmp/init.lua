1a369772d765c1f615233b20a9bad932b0196805
Wassim Metallaoui <wassim@metallaoui.com>
2022-07-10 17:24:00 -0500

    feat: make c-p and c-n cycle choice nodes in luasnip

--- config/nvim/lua/wassim/cmp/init.lua
+++ config/nvim/lua/wassim/cmp/init.lua
@@ -5,12 +5,25 @@ if has_cmp then
   local lspkind = require('lspkind')
 
   local key_mappings = {
-    ['<C-p>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }),
-    ['<C-n>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
-    ['<C-u>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-    ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-    ['<C-y>'] = cmp.mapping(cmp.mapping.confirm(), { 'i', 'c' }),
-    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+    ['<C-p>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif has_luasnip and luasnip.choice_active() then
+        luasnip.change_choice(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 'c' }),
+
+    ['<C-n>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif has_luasnip and luasnip.choice_active() then
+        luasnip.change_choice(1)
+      else
+        fallback()
+      end
+    end, { 'i', 'c' }),
 
     ['<C-u>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
@@ -21,6 +34,11 @@ if has_cmp then
         fallback()
       end
     end, { 'i', 's' }),
+
+    ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+    ['<C-y>'] = cmp.mapping(cmp.mapping.confirm(), { 'i', 'c' }),
+    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+    ['<C-e>'] = cmp.mapping(cmp.mapping.abort(), { 'i', 'c' }),
   }
 
   cmp.setup {
