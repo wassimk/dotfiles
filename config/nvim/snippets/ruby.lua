08090c87fc0f9537886f795097cdfa6cafa417af
Wassim Metallaoui <wassim@metallaoui.com>
2022-07-11 20:07:38 -0500

    feat: get cassette names and add as choice nodes

--- config/nvim/snippets/ruby.lua
+++ config/nvim/snippets/ruby.lua
@@ -6,6 +6,28 @@ if not has_luasnip then
   return
 end
 
+-- TODO: don't require plenary, can do this with Lua easier because we don't need folder depth
+local cassettes = function()
+  local cassettes_dir = './test/vcr_cassettes'
+
+  local files = require('plenary.scandir').scan_dir(cassettes_dir, { search_pattern = '.yml', respect_gitignore = true, depth = 1, silent = true })
+
+  local cassettes = {}
+  for _, file in ipairs(files) do
+    local cassette = file:match(cassettes_dir .. '/stripe%-(.+)%.yml$')
+
+    if cassette ~= nil then
+      table.insert(cassettes, t(cassette))
+    end
+  end
+
+  if not next(cassettes) then
+    return { t('unable to load cassette choices from ' .. cassettes_dir) }
+  else
+    return cassettes
+  end
+end
+
 return {
   s(
     {
@@ -24,7 +46,7 @@ return {
   s(
     {
       trig = 'usca',
-      name = 'use_stripe_cassette',
+      name = 'use_stripe_cassette (advanced)',
       dscr = 'Stripe VCR cassette with object from cassette data',
       hidden = false,
     },
@@ -34,13 +56,13 @@ return {
           {} = stripe_object_from_cassette(cassette, path: "{}", http_method: :{})
           {}
         end
-      ]], { i(1, 'cassette-name'), i(2, 'stripe_object_name'), i(3, 'request-path'), c(4, { t 'get', t 'post', t 'put', t 'delete' }), i(0) }
+      ]], { c(1, cassettes()), i(2, 'stripe_object_name'), i(3, 'request-path'), c(4, { t 'get', t 'post', t 'put', t 'delete' }), i(0) }
     )
   ),
   s(
     {
       trig = 'uscs',
-      name = 'use_stripe_cassette',
+      name = 'use_stripe_cassette (simple)',
       dscr = 'Stripe VCR cassette',
       hidden = false,
     },
@@ -49,7 +71,7 @@ return {
         use_stripe_cassette("{}") do
           {}
         end
-      ]], { i(1), i(0) }
+      ]], { c(1, cassettes()), i(0) }
     )
   )
 }
