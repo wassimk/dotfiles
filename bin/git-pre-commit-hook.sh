5413779da04d142b089bb0f00a0e1c05109db383
Wassim Metallaoui <wassim@metallaoui.com>
2022-07-29 11:13:48 -0500

    chore: misc script cleanup/documentation

--- bin/git-pre-commit-hook.sh
+++ bin/git-pre-commit-hook.sh
@@ -7,6 +7,7 @@
 # Check all staged Ruby (*.rb/haml/coffee) files
 # for Pry binding references
 # https://gist.github.com/alexbevi/3436040
+
 FILES='(js|css|rb)'
 FORBIDDEN='(binding.pry|console.log|\!important)'
 GREP_COLOR='4;5;37;41'
@@ -16,17 +17,6 @@ if [[ $(git diff --cached --name-only | grep -E $FILES) ]]; then
   xargs grep --color --with-filename -n -E $FORBIDDEN && \
   echo "Looks like you are trying to commit something you shouldn't.  Please fix your diff, or run 'git commit --no-verify' to skip this check, if you must." && \
   exit 1
-fi
-
-# Run Test Suite
-# If file exists and is executable
-if [ -x ./run_tests.sh ]; then
-  echo "Running tests via pre-commit hook"
-  git stash -q --keep-index
-  ./run_tests.sh
-  RESULT=$?
-  git stash pop -q
-  [ $RESULT -ne 0 ] && exit 1
 fi
 
 # Get this far then nothing failed so exit clean
