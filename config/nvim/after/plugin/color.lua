d7f22a05c3273e0d5453ddff7f3dedf1e18c494b
Wassim Metallaoui <wassim@metallaoui.com>
2022-07-30 10:27:10 -0500

    feat: add border around diagnostic floats
    What I really want to do is hide the cursorline and not have this
    border. But, this will do for now.

--- config/nvim/after/plugin/color.lua
+++ config/nvim/after/plugin/color.lua
@@ -20,3 +20,14 @@ for type, icon in pairs(signs) do
 
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
 end
+
+-- add border to lsp floats, globally
+-- TODO: remove border and disable cursorline
+local border = 'single'
+
+local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+  opts = opts or {}
+  opts.border = opts.border or border
+  return orig_util_open_floating_preview(contents, syntax, opts, ...)
+end
