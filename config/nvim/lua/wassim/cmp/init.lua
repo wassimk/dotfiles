aad47e5dff3194326b2ff9e75ea4d038dc5a7412
Wassim Metallaoui <wassim@metallaoui.com>
2022-07-30 10:27:10 -0500

    feat: add border around diagnostic floats
    What I really want to do is hide the cursorline and not have this
    border. But, this will do for now.

--- config/nvim/lua/wassim/cmp/init.lua
+++ config/nvim/lua/wassim/cmp/init.lua
@@ -51,8 +51,8 @@ if has_cmp then
     },
 
     window = {
-      -- completion = cmp.config.window.bordered(),
-      -- documentation = cmp.config.window.bordered(),
+      -- completion = cmp.config.window.bordered({ border = 'single' }),
+      -- documentation = cmp.config.window.bordered({ border = 'single' }),
     },
 
     formatting = {
