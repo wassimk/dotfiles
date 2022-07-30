050e5934ae35de21ef9725f7db07d921a1e0008a
Wassim Metallaoui <wassim@metallaoui.com>
2022-07-30 18:04:41 -0500

    feat: prettier plugin run on save if config file detected
    I think this is still a problem for apps that run eslint prettier and
    have a configuration, but, we'll tackle that next.

--- config/nvim/ftplugin/ruby.lua
+++ config/nvim/ftplugin/ruby.lua
@@ -1,6 +1,3 @@
-local wamGrp = vim.api.nvim_create_augroup('WamAutocmdsRubyFormatting', { clear = true })
-
-vim.api.nvim_create_autocmd('BufWritePre', {
-  command = 'silent! PrettierAsync',
-  group = wamGrp,
-})
+--
+-- ruby filetype
+--
