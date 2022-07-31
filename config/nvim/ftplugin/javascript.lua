7f1a8e5969dd5f9deae558235a2ac5d6236db73a
Wassim Metallaoui <wassim@metallaoui.com>
2022-07-31 16:38:50 -0500

    fix: don't automatically fix imports or organization them
    Too many projects don't do this right now. If I want to run any of them
    they are in :Typescript* commands.

--- config/nvim/ftplugin/javascript.lua
+++ config/nvim/ftplugin/javascript.lua
@@ -6,8 +6,8 @@ local auGroup = vim.api.nvim_create_augroup('WamAutocmdsJavaScriptFormatting', {
 
 vim.api.nvim_create_autocmd('BufWritePre', {
   callback = function()
-    require('typescript').actions.addMissingImports({ sync = true })
-    require('typescript').actions.organizeImports({ sync = true })
+    -- require('typescript').actions.addMissingImports({ sync = true })
+    -- require('typescript').actions.organizeImports({ sync = true })
     require('typescript').actions.removeUnused({ sync = true })
     require('typescript').actions.fixAll({ sync = true })
 
