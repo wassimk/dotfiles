ebe6ade049e1d0674cc177a9a00e2d16e5d8bc50
Wassim Metallaoui <wassim@metallaoui.com>
2022-07-30 19:17:41 -0500

    feat: switch prettier formatting to null-ls and use prettierd

--- config/nvim/ftplugin/ruby.lua
+++ config/nvim/ftplugin/ruby.lua
@@ -1,3 +1,10 @@
 --
 -- ruby filetype
 --
+
+vim.api.nvim_create_autocmd('BufWritePre', {
+  callback = function()
+    vim.lsp.buf.formatting_seq_sync()
+  end,
+  group = vim.api.nvim_create_augroup('WamAutocmdsRubyFormatting', {}),
+})
