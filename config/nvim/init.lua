41b46b797928db5aa5c1663c6f28fd531844ccdc
Wassim Metallaoui <wassim@metallaoui.com>
2022-08-01 19:11:48 -0500

    fix: workaround an issue with todo-comments.nvim highlights
    References:
    https://github.com/folke/todo-comments.nvim/issues/97

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -116,6 +116,14 @@ require('Comment').setup()
 require('trouble').setup()
 
 -- todo-comments.nvim
+-- NOTE: workaround until issue is fixed upstream
+-- https://github.com/folke/todo-comments.nvim/issues/97
+local hl = require('todo-comments.highlight')
+local highlight_win = hl.highlight_win
+hl.highlight_win = function(win, force)
+  pcall(highlight_win, win, force)
+end
+
 require('todo-comments').setup({
   sign_priority = 5, -- lower than gitsigns
 })
