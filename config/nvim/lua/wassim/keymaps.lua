51ffa5d4c5d46315081baa71d13c38f58bc83828
Wassim Metallaoui <wassim@metallaoui.com>
2022-08-01 19:46:50 -0500

    chore: rename mappings to keymaps and drop unused mappings

--- config/nvim/lua/wassim/keymaps.lua
+++ config/nvim/lua/wassim/keymaps.lua
@@ -2,19 +2,12 @@
 -- Key Mappings
 --------------
 
--- custom leader mapping
-vim.cmd('let mapleader = "\\<Space>"')
+vim.g.mapleader = ' '
 
 local opts = { silent = true }
 
--- nvim-tree toggle
+-- nvim-tree
 vim.keymap.set('n', '<C-n>', '<cmd>NvimTreeToggle<CR>', opts)
-
--- edit configuration files
-vim.keymap.set('n', '<leader>ev', '<cmd>vsplit $MYVIMRC<CR>', opts)
-vim.keymap.set('n', '<leader>ez', '<cmd>vsplit ~/.zshrc<CR>', opts)
-vim.keymap.set('n', '<leader>et', '<cmd>vsplit ~/.tmux.conf<CR>', opts)
-vim.keymap.set('n', '<leader>sv', '<cmd>source $MYVIMRC<CR>', opts)
 
 -- vim-test
 vim.keymap.set('n', 't<C-n>', '<cmd>TestNearest<CR>', opts)
@@ -36,25 +29,21 @@ vim.keymap.set('n', '<C-f>h', '<cmd>Telescope help_tags<CR>', opts)
 vim.keymap.set('n', '<C-f>c', '<cmd>Telescope commands<CR>', opts)
 vim.keymap.set('n', '<C-f>k', '<cmd>Telescope keymaps<CR>', opts)
 
--- split resizing (taken over by yabai)
+require('telescope').setup({
+  defaults = {
+    mappings = {
+      i = { ['<C-y>'] = require('telescope.actions').select_default },
+      n = { ['<C-y>'] = require('telescope.actions').select_default },
+    },
+  },
+})
+
+-- bufferline.nvim
+vim.keymap.set('n', ']b', '<cmd>BufferLineCycleNext<CR>', opts)
+vim.keymap.set('n', '[b', '<cmd>BufferLineCyclePrev<CR>', opts)
+
+-- split resizing (defaults taken over by yabai)
 vim.keymap.set('n', '<M-i>', '<cmd>resize -5<CR>')
 vim.keymap.set('n', '<M-u>', '<cmd>resize +5<CR>')
 vim.keymap.set('n', '<M-y>', '<cmd>vertical resize +5<CR>')
 vim.keymap.set('n', '<M-o>', '<cmd>vertical resize -5<CR>')
-
-local actions = require('telescope.actions')
-require('telescope').setup({
-  defaults = {
-    mappings = {
-      i = {
-        ['<C-y>'] = actions.select_default,
-      },
-      n = {
-        ['<C-y>'] = actions.select_default,
-      },
-    },
-  },
-})
--- bufferline
-vim.keymap.set('n', ']b', '<cmd>BufferLineCycleNext<CR>', opts)
-vim.keymap.set('n', '[b', '<cmd>BufferLineCyclePrev<CR>', opts)
