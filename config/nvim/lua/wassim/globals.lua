bb4d724a5edcffc1274d3d923159a8da161f532c
Wassim Metallaoui <wassim@metallaoui.com>
2022-08-06 09:51:35 -0500

    fix: improve globals with plenary check and fix linter warnings

--- config/nvim/lua/wassim/globals.lua
+++ config/nvim/lua/wassim/globals.lua
@@ -1,15 +1,23 @@
+local reloader = nil
+local ok, plenary_reload = pcall(require, 'plenary.reload')
+if not ok then
+  reloader = require
+else
+  reloader = plenary_reload.reload_module
+end
+
 P = function(v)
   print(vim.inspect(v))
   return v
 end
 
 LOG = function(...)
-  logger = require('plenary.log').new({ plugin = 'globals-log' })
+  local logger = require('plenary.log').new({ plugin = 'globals-log' })
   return logger.info(...)
 end
 
 RELOAD = function(...)
-  return require('plenary.reload').reload_module(...)
+  return reloader(...)
 end
 
 R = function(name)
