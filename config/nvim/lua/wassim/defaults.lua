c03f175f165aa35b99bf808759ffbec018faf33c
Wassim Metallaoui <wassim@metallaoui.com>
2022-08-06 09:55:55 -0500

    feat: always show sign column to avoid load jitter

--- config/nvim/lua/wassim/defaults.lua
+++ config/nvim/lua/wassim/defaults.lua
@@ -10,6 +10,7 @@ opt.number = true -- display line numbers beside buffer
 opt.relativenumber = true -- show line numbers relative to cursor position
 opt.scrolloff = 4 -- keep at least 4 lines below cursor
 opt.shortmess = opt.shortmess + 'a' -- some sane display defaults
+opt.signcolumn = 'yes' -- always show sign column to avoid visual change
 opt.splitbelow = true -- new horizontal split below
 opt.splitright = true -- new vertical splits to the right
 opt.swapfile = false -- don't create useless swap files
@@ -18,10 +19,10 @@ opt.undofile = true -- allow undo even after closing a file
 opt.winwidth = 100 -- set minimum width of current window
 
 -- preferred default tabs/spaces
+opt.expandtab = true
 opt.shiftwidth = 2
 opt.softtabstop = 2
 opt.tabstop = 2
-opt.expandtab = true
 
 -- use only the system clipboard
 opt.clipboard = opt.clipboard + 'unnamedplus'
