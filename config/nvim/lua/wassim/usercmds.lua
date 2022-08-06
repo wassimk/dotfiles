e2541ba115a78a1336b540251e06211cb3821c5b
Wassim Metallaoui <wassim@metallaoui.com>
2022-08-06 11:02:45 -0500

    feat: add usercmd that gets called when vim-conflicted starts

--- config/nvim/lua/wassim/usercmds.lua
+++ config/nvim/lua/wassim/usercmds.lua
@@ -1,4 +1,11 @@
 local api = vim.api
 
--- user command to launch lazygit in a floaterm
-api.nvim_create_user_command('Lg', 'FloatermNew lazygit', {})
+api.nvim_create_user_command('Lg', 'FloatermNew lazygit', { desc = 'launch lazygit with floaterm' })
+
+api.nvim_create_user_command('VimConflicted', function()
+  -- add conflicted version name to statusline
+  vim.wo.statusline = vim.wo.statusline .. '%{ConflictedVersion()}'
+
+  -- resolve and move to next conflicted file
+  vim.keymap.set('n', ']m', 'GitNextConflict', { noremap = true, silent = true })
+end, { desc = 'vim-conflicted calls when entering conflicted state' })
