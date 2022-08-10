b927915e6c0f579dfe8a3222d5b5c0868b359ba0
Wassim Metallaoui <wassim@metallaoui.com>
2022-08-10 18:14:56 -0500

    chore: remove vim-conflicted, not stable in neovim

--- config/nvim/lua/wassim/usercmds.lua
+++ config/nvim/lua/wassim/usercmds.lua
@@ -1,11 +1,3 @@
 local api = vim.api
 
 api.nvim_create_user_command('Lg', 'FloatermNew lazygit', { desc = 'launch lazygit with floaterm' })
-
-api.nvim_create_user_command('VimConflicted', function()
-  -- add conflicted version name to statusline
-  vim.wo.statusline = vim.wo.statusline .. '%{ConflictedVersion()}'
-
-  -- resolve and move to next conflicted file
-  vim.keymap.set('n', ']m', 'GitNextConflict', { noremap = true, silent = true })
-end, { desc = 'vim-conflicted calls when entering conflicted state' })
