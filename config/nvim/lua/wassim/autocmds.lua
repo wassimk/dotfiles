854695ee7b3f1e058a5d82d592e7b159e9f564cc
Wassim Metallaoui <wassim@metallaoui.com>
2022-08-09 20:52:31 -0500

    feat: make the yank highlight a little faster

--- config/nvim/lua/wassim/autocmds.lua
+++ config/nvim/lua/wassim/autocmds.lua
@@ -3,7 +3,7 @@ local wamGrp = api.nvim_create_augroup('WamAutocmds', {})
 
 -- highlight on yank
 api.nvim_create_autocmd('TextYankPost', {
-  command = 'silent! lua vim.highlight.on_yank { timeout = 750 }',
+  command = 'silent! lua vim.highlight.on_yank { timeout = 500 }',
   group = wamGrp,
 })
 
