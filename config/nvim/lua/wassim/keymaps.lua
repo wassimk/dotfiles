4cf487575db7bb4845c4747b1715fb82464892b3
Wassim Metallaoui <wassim@metallaoui.com>
2022-08-17 09:36:37 -0500

    feat: keymap for easier to yanking into system clipboard

--- config/nvim/lua/wassim/keymaps.lua
+++ config/nvim/lua/wassim/keymaps.lua
@@ -6,6 +6,9 @@ vim.api.nvim_set_keymap('', '<Space>', '<Nop>', { noremap = true, silent = true 
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- clipboard
+keymap({ 'n', 'v' }, '<Leader>y', '"+y', opts)
+
 -- nvim-tree
 keymap('n', '<C-n>', '<cmd>NvimTreeToggle<CR>', opts)
 
