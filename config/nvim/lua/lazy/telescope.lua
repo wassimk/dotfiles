05110d426564581b6195579e2f8da201297f230d
Wassim Metallaoui <wassim@metallaoui.com>
2022-08-19 11:44:49 -0500

    feat: setup nvim-dap and helper tools

--- config/nvim/lua/lazy/telescope.lua
+++ config/nvim/lua/lazy/telescope.lua
@@ -6,6 +6,7 @@
 local M = {}
 
 function M.setup()
+  require('telescope').load_extension('dap')
   require('telescope').load_extension('fzf')
   require('telescope').load_extension('ui-select')
 end
