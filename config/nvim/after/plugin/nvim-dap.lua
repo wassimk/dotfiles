95c413c054ee6395890a1141d9263f957c5aaf65
Wassim Metallaoui <wassim@metallaoui.com>
2022-08-19 17:57:06 -0500

    fix: the event listener launching of dapui crashes
    So, for now we'll use a keymap to toggle it open/closed.

--- config/nvim/after/plugin/nvim-dap.lua
+++ config/nvim/after/plugin/nvim-dap.lua
@@ -10,7 +10,7 @@ local dap, dapui, dap_virtual_text = require('dap'), require('dapui'), require('
 dapui.setup()
 dap_virtual_text.setup()
 
--- keymaps
+-- dap keymaps
 keymap('n', '<F5>', dap.continue, opts)
 keymap('n', '<F10>', dap.step_over, opts)
 keymap('n', '<F11>', dap.step_into, opts)
@@ -26,12 +26,5 @@ keymap(
 keymap('n', '<Leader>dr', dap.repl.open, opts)
 keymap('n', '<Leader>dl', dap.run_last, opts)
 
-dap.listeners.after.event_initialized['dapui_config'] = function()
-  dapui.open()
-end
-dap.listeners.before.event_terminated['dapui_config'] = function()
-  dapui.close()
-end
-dap.listeners.before.event_exited['dapui_config'] = function()
-  dapui.close()
-end
+-- dapui keymap
+keymap('n', '<Leader>du', dapui.toggle, opts)
