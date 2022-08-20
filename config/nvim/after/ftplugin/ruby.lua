c2eac58780ab324ad448146b254bb41d819944f0
Wassim Metallaoui <wassim@metallaoui.com>
2022-08-20 12:48:06 -0500

    feat: configure ruby dap using ruby/debug
    Now we can run:
    
    - ruby script
    - ruby script with arguments
    - minitest file
    - minitest neartest

--- config/nvim/after/ftplugin/ruby.lua
+++ config/nvim/after/ftplugin/ruby.lua
@@ -14,26 +14,31 @@ vim.api.nvim_create_autocmd('BufWritePre', {
 local dap = require('dap')
 
 dap.adapters.ruby = function(callback, config)
+  local args = {
+    'exec',
+    'rdbg',
+    '--open',
+    '--nonstop',
+    '--port',
+    '${port}',
+    '--command',
+    '--',
+  }
+
+  vim.list_extend(args, config.command)
+  vim.list_extend(args, config.script)
+
+  if config.script_args then
+    vim.list_extend(args, config.script_args)
+  end
+
   callback({
     type = 'server',
     host = '127.0.0.1',
     port = '${port}',
     executable = {
       command = 'bundle',
-      args = {
-        'exec',
-        'rdbg',
-        '-n',
-        '--open',
-        '--port',
-        '${port}',
-        '-c',
-        '--',
-        'bundle',
-        'exec',
-        config.command,
-        config.script,
-      },
+      args = args,
     },
   })
 end
@@ -41,26 +46,41 @@ end
 dap.configurations.ruby = {
   {
     type = 'ruby',
-    name = 'debug current file',
+    name = 'debug file',
     request = 'attach',
     localfs = true,
-    command = 'ruby',
-    script = '${file}',
+    command = { 'bundle', 'exec', 'ruby' },
+    script = { '${relativeFile}' },
   },
   {
     type = 'ruby',
-    name = 'run current spec file',
+    name = 'debug file (prompt)',
     request = 'attach',
     localfs = true,
-    command = 'rspec',
-    script = '${file}',
+    command = { 'bundle', 'exec', 'ruby' },
+    script = { '${relativeFile}' },
+    script_args = function()
+      local argument_string = vim.fn.input('Program arguments: ')
+      return vim.fn.split(argument_string, ' ', true)
+    end,
   },
   {
     type = 'ruby',
-    name = 'run current test file',
+    name = 'debug test file',
     request = 'attach',
     localfs = true,
-    command = 'rails test',
-    script = '${file}',
+    command = { 'bundle', 'exec', 'ruby' },
+    script = { 'bin/rails', 'test', '${relativeFile}' },
+  },
+  {
+    type = 'ruby',
+    name = 'debug test nearest',
+    request = 'attach',
+    localfs = true,
+    command = { 'bundle', 'exec', 'ruby' },
+    script = function()
+      local line = vim.fn.line('.')
+      return { 'bin/rails', 'test', '${relativeFile}:' .. line }
+    end,
   },
 }
