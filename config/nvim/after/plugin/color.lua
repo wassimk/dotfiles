2b3153b1f49ed15cf15f79632e9ecacf12dc8fd8
Wassim Metallaoui <wassim@metallaoui.com>
2022-08-31 07:35:50 -0500

    feat: move sign icons for dap into config

--- config/nvim/after/plugin/color.lua
+++ config/nvim/after/plugin/color.lua
@@ -11,20 +11,6 @@ vim.api.nvim_set_hl(0, 'MatchWord', { italic = true })
 vim.api.nvim_set_hl(0, 'WinBarPath', { bg = '#282c34' })
 vim.api.nvim_set_hl(0, 'WinBarModified', { bg = '#282c34' })
 
-local dap_signs = {
-  Breakpoint = 'ﰉ',
-  BreakpointCondition = 'ﰊ',
-  LogPoint = 'ﯶ',
-  Stopped = '',
-  BreakpointRejected = 'ﰸ',
-}
-
-for type, icon in pairs(dap_signs) do
-  local hl = 'Dap' .. type
-
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
-end
-
 -- add border to lsp floats, globally
 -- TODO: remove border and disable cursorline
 local border = 'single'
