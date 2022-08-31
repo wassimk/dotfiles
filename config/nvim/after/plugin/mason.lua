4a50daeb2214ec7fed491d07ac8b3d7683fcaac6
Wassim Metallaoui <wassim@metallaoui.com>
2022-08-31 07:31:53 -0500

    feat: move mason out of lsp configuration

---
+++ config/nvim/after/plugin/mason.lua
@@ -0,0 +1,28 @@
+--
+-- mason.nvim, mason-lspconfig.nvim, mason-tool-installer.nvim
+-- https://github.com/williamboman/mason.nvim
+-- https://github.com/williamboman/mason-lspconfig.nvim
+-- https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim
+
+require('mason').setup()
+
+-- automatic lsp server installs
+require('mason-lspconfig').setup({
+  automatic_installation = {
+    exclude = { 'solargraph', 'ruby-lsp', 'syntax_tree' },
+  },
+})
+
+-- mason auto-update
+require('mason-tool-installer').setup({
+  ensure_installed = {
+    'codespell',
+    'prettierd',
+    'selene',
+    'shellcheck', -- used by bash-language-server
+    'stylua',
+  },
+  auto_update = true,
+  run_on_start = true,
+  start_delay = 5000,
+})
