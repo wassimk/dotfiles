4cb9f4d51b7888c0111fd7e334cd888913ef4738
Wassim Metallaoui <wassim@metallaoui.com>
2022-12-05 08:21:08 -0600

    fix: removeUnused in javascript is never helpful to me

--- config/nvim/after/ftplugin/typescriptreact.lua
+++ config/nvim/after/ftplugin/typescriptreact.lua
@@ -8,7 +8,7 @@ vim.api.nvim_create_autocmd('BufWritePre', {
   callback = function()
     -- require('typescript').actions.addMissingImports({ sync = true })
     -- require('typescript').actions.organizeImports({ sync = true })
-    require('typescript').actions.removeUnused({ sync = true })
+    -- require('typescript').actions.removeUnused({ sync = true })
     require('typescript').actions.fixAll({ sync = true })
 
     vim.lsp.buf.format()
