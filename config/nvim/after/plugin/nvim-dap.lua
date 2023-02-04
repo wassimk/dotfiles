491afc677cbb974f83a45995fe4a3175e38d3e82
Wassim Metallaoui <wassim@metallaoui.com>
2023-02-04 17:58:50 -0600

    feat: automatically launch dap-ui when debugging

--- config/nvim/after/plugin/nvim-dap.lua
+++ config/nvim/after/plugin/nvim-dap.lua
@@ -52,6 +52,18 @@ dapui.setup({
 
 keymap('n', '<Leader>du', dapui.toggle, opts)
 
+dap.listeners.after.event_initialized['dapui_config'] = function()
+  dapui.open()
+end
+
+dap.listeners.before.event_terminated['dapui_config'] = function()
+  dapui.close()
+end
+
+dap.listeners.before.event_exited['dapui_config'] = function()
+  dapui.close()
+end
+
 -- dap-virtual-text
 local dap_virtual_text = require('nvim-dap-virtual-text')
 
