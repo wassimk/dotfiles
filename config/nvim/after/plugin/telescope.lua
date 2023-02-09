6e0d6d66dcd9e9cee604a274d44659842bd128e3
Wassim Metallaoui <wassim@metallaoui.com>
2023-02-08 20:51:03 -0600

    feat: prompts in telescope for extension and folder scoping
    <C-l> while in a Telescope live grep will prompt for a "l"ist of
    folders to scope the search by. You can pick multiple with tab.
    
    <C-f> will prompt to further filter by "f"ile type.
    
    References:
    https://www.reddit.com/r/neovim/comments/xj784v/telescope_live_grep_inside_certain_folders/
    https://github.com/JoosepAlviste/dotfiles/blob/180b97c15c02181faa4fe80a081199626a050cb0/config/nvim/lua/j/plugins/telescope.lua#L6

--- config/nvim/after/plugin/telescope.lua
+++ config/nvim/after/plugin/telescope.lua
@@ -4,10 +4,7 @@
 --
 
 local telescope, builtin, actions = require('telescope'), require('telescope.builtin'), require('telescope.actions')
-
-telescope.load_extension('dap')
-telescope.load_extension('fzf')
-telescope.load_extension('ui-select')
+local custom_pickers = require('wassim.telescope_custom_pickers')
 
 telescope.setup({
   defaults = {
@@ -22,7 +19,21 @@ telescope.setup({
       },
     },
   },
+  pickers = {
+    live_grep = {
+      mappings = {
+        i = {
+          ['<C-f>'] = custom_pickers.actions.set_extension,
+          ['<C-l>'] = custom_pickers.actions.set_folders,
+        },
+      },
+    },
+  },
 })
+
+telescope.load_extension('dap')
+telescope.load_extension('fzf')
+telescope.load_extension('ui-select')
 
 -- keymaps
 local opts = { noremap = true, silent = true }
@@ -30,7 +41,7 @@ local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<C-f>', '', opts) -- i keep hitting this and it scrolls the screen!
 vim.keymap.set('n', '<C-f>f', builtin.find_files, opts)
 vim.keymap.set('n', '<C-f>w', builtin.grep_string, opts)
-vim.keymap.set('n', '<C-f>g', builtin.live_grep, opts)
+vim.keymap.set('n', '<C-f>g', custom_pickers.live_grep, opts)
 vim.keymap.set('n', '<C-f>b', builtin.current_buffer_fuzzy_find, opts)
 vim.keymap.set('n', '<C-f>h', builtin.help_tags, opts)
 vim.keymap.set('n', '<C-f>c', builtin.commands, opts)
