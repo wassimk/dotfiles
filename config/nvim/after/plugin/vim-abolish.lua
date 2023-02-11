bf7b90bcff25b48f50b70a3ca8e26c15a92fc982
Wassim Metallaoui <wassim@metallaoui.com>
2023-02-10 21:04:54 -0600

    fix: there is nothing to require in lua here

--- config/nvim/after/plugin/vim-abolish.lua
+++ config/nvim/after/plugin/vim-abolish.lua
@@ -3,12 +3,6 @@
 -- https://github.com/tpope/vim-abolish
 --
 
-local has_abolish = pcall(require, 'vim-abolish')
-
-if not has_abolish then
-  return
-end
-
 vim.cmd([[
   Abolish precense presence
   Abolish {despa,sepe}rat{e,es,ed,ing,ely,ion,ions,or}  {despe,sepa}rat{}
