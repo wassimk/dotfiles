404758c186996c198db07241ecfb1abeec2bc500
Wassim Metallaoui <wassim@metallaoui.com>
2023-02-15 10:06:40 -0600

    fix: don't try to format .html.erb files

--- config/nvim/after/ftplugin/ruby.lua
+++ config/nvim/after/ftplugin/ruby.lua
@@ -4,7 +4,11 @@
 
 -- format on save
 vim.api.nvim_create_autocmd('BufWritePre', {
-  callback = function()
+  callback = function(event)
+    if vim.endswith(event.file, '.html.erb') then
+      return
+    end
+
     vim.lsp.buf.format()
   end,
   group = vim.api.nvim_create_augroup('WamAutocmdsRubyFormatting', {}),
