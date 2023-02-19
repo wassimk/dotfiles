3ffb536825d6928f62dec8d739b11b7e5ae43908
Wassim Metallaoui <wassim@metallaoui.com>
2023-02-18 19:17:15 -0600

    refactor: move javascript lsp config to own module

---
+++ config/nvim/lua/w/lsp/javascript.lua
@@ -0,0 +1,40 @@
+--
+-- javascript lsp
+--
+
+local M = {}
+
+function M.setup()
+  local capabilities = require('w.lsp').capabilities()
+  local on_attach = require('w.lsp').on_attach
+
+  -- javascript / typescript
+  -- this plugin calls lspconfig and sets up tsserver
+  require('typescript').setup({
+    disable_commands = false, -- :Typescript* commands
+    debug = false,
+    server = {
+      capabilities = capabilities,
+      on_attach = on_attach,
+    },
+  })
+
+  -- eslint
+  require('lspconfig').eslint.setup({
+    settings = {
+      validate = 'on',
+      codeAction = {
+        disableRuleComment = {
+          location = 'separateLine',
+        },
+        showDocumentation = {
+          enable = true,
+        },
+      },
+    },
+    capabilities = capabilities,
+    on_attach = on_attach,
+  })
+end
+
+return M
