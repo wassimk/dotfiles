75c8c3622e464088625ed5577cc485fd459266fa
Wassim Metallaoui <wassim@metallaoui.com>
2023-02-18 19:11:09 -0600

    refactor: move rust lsp config to filetype

--- config/nvim/lua/w/legacy_lsp.lua
+++ config/nvim/lua/w/legacy_lsp.lua
@@ -38,32 +38,3 @@ require('lspconfig').eslint.setup({
   capabilities = capabilities,
   on_attach = on_attach,
 })
-
-----
--- rust
-----
--- use library from vscode CodeLLDB extension
-local extension_path = vim.env.HOME .. '/.vscode/extensions/vadimcn.vscode-lldb-1.7.4/'
-local codelldb_path = extension_path .. 'adapter/codelldb'
-local liblldb_path = extension_path .. 'lldb/lib/liblldb.dylib'
-
-require('dap').adapters.lldb = {
-  type = 'executable',
-  attach = { pidProperty = 'pid', pidSelect = 'ask' },
-  command = 'lldb-vscode',
-  name = 'lldb',
-  env = { LLDB_LAUNCH_FLAG_LAUNCH_IN_TTY = 'YES' },
-}
-
-local opts = {
-  dap = {
-    adapter = require('rust-tools.dap').get_codelldb_adapter(codelldb_path, liblldb_path),
-  },
-  server = {
-    capabilities = capabilities,
-    on_attach = on_attach,
-  },
-}
-
--- this plugin calls lspconfig and sets up rust-analyzer and nvim-dap
-require('rust-tools').setup(opts)
