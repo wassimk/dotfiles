13affa216ddcf30ea797e03a1c3ddb39ada1da50
Wassim Metallaoui <wassim@metallaoui.com>
2023-02-18 19:12:42 -0600

    refactor: move vim lsp config to filetype

---
+++ config/nvim/after/ftplugin/vim.lua
@@ -0,0 +1,9 @@
+--
+-- vim filetype
+--
+
+require('lspconfig').vimls.setup({
+  init_options = { isNeovim = true },
+  capabilities = require('w.lsp').capabilities(),
+  on_attach = require('w.lsp').on_attach,
+})
