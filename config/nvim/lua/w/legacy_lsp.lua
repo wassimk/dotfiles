79006bdd05b0ef1c9ffba0b4625fa1ad9bd574ae
Wassim Metallaoui <wassim@metallaoui.com>
2023-02-18 19:12:42 -0600

    refactor: move vim lsp config to filetype

--- config/nvim/lua/w/legacy_lsp.lua
+++ config/nvim/lua/w/legacy_lsp.lua
@@ -4,13 +4,6 @@
 local capabilities = require('w.lsp').capabilities()
 local on_attach = require('w.lsp').on_attach
 
--- vimscript
-require('lspconfig').vimls.setup({
-  init_options = { isNeovim = true },
-  capabilities = capabilities,
-  on_attach = on_attach,
-})
-
 -- javascript / typescript
 -- this plugin calls lspconfig and sets up tsserver
 require('typescript').setup({
