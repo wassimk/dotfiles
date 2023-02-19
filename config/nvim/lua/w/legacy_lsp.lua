b0919c08c742ee847bfd31b0fe6b05e2584a18ba
Wassim Metallaoui <wassim@metallaoui.com>
2023-02-18 18:46:15 -0600

    refactor: move on_attach and capabilities to module

--- config/nvim/lua/w/legacy_lsp.lua
+++ config/nvim/lua/w/legacy_lsp.lua
@@ -3,37 +3,9 @@
 ----
 local utils = require('w.utils')
 
-local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('w.lsp').capabilities()
 
-local on_attach = function(client)
-  local opts = { buffer = 0, silent = true }
-
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-  vim.keymap.set('n', 'gS', vim.lsp.buf.signature_help, opts)
-  vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-  vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-  vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, opts)
-  vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
-  vim.keymap.set('n', '<leader>de', vim.diagnostic.open_float, opts)
-  vim.keymap.set('n', '<leader>ds', vim.diagnostic.setloclist, opts)
-  vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, opts)
-
-  if client.name == 'rust_analyzer' then
-    vim.keymap.set({ 'n', 'v' }, '<Leader>ca', require('rust-tools').code_action_group.code_action_group, opts)
-    vim.keymap.set('n', 'K', require('rust-tools').hover_actions.hover_actions, opts)
-  else
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
-  end
-
-  if client.name == 'tsserver' then
-    client.server_capabilities.document_formatting = false
-    client.server_capabilities.document_range_formatting = false
-  end
-end
+local on_attach = require('w.lsp').on_attach
 
 -- bash scripting
 require('lspconfig').bashls.setup({ capabilities = capabilities, on_attach = on_attach })
