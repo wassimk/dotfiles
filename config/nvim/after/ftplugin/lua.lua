574924e8e6caae09e406dbf4b577cc4db0e333b6
Wassim Metallaoui <wassim@metallaoui.com>
2023-02-18 18:51:07 -0600

    refactor: move lua lsp config to filetype

--- config/nvim/after/ftplugin/lua.lua
+++ config/nvim/after/ftplugin/lua.lua
@@ -2,6 +2,28 @@
 -- lua filetype
 --
 
+local capabilities = require('w.lsp').capabilities()
+local on_attach = require('w.lsp').on_attach
+
+require('neodev').setup({
+  library = {
+    plugins = { 'nvim-treesitter', 'plenary.nvim', 'nvim-dap', 'gitsigns.nvim', 'nvim-dap-ui' },
+  },
+})
+
+require('lspconfig').lua_ls.setup({
+  settings = {
+    Lua = {
+      diagnostics = { globals = { 'vim', 'hs', 'packer_plugins' } },
+      format = { enable = false },
+      telemetry = { enable = false },
+    },
+  },
+  capabilities = capabilities,
+  on_attach = on_attach,
+})
+
+-- format on save
 vim.api.nvim_create_autocmd('BufWritePre', {
   callback = function()
     vim.lsp.buf.format()
