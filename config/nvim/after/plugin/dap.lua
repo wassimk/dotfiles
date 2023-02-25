e6d538092e98535f5f09f76bbeff2cd12aeda1bd
Wassim Metallaoui <wassim@metallaoui.com>
2023-02-25 10:42:49 -0600

    feat: don't auto-close dap-ui

--- config/nvim/after/plugin/dap.lua
+++ config/nvim/after/plugin/dap.lua
@@ -66,14 +66,6 @@ if has_dapui then
   dap.listeners.after.event_initialized['dapui_config'] = function()
     dapui.open()
   end
-
-  dap.listeners.before.event_terminated['dapui_config'] = function()
-    dapui.close()
-  end
-
-  dap.listeners.before.event_exited['dapui_config'] = function()
-    dapui.close()
-  end
 end
 
 -- dap-virtual-text
