26d93b76db501a62952619be7305f4309f6f2a88
Wassim Metallaoui <wassim@metallaoui.com>
2023-02-26 13:17:46 -0600

    refactor: remove extra space on diagnostic icons and fix keymap

--- config/nvim/lua/w/diagnostic.lua
+++ config/nvim/lua/w/diagnostic.lua
@@ -20,16 +20,16 @@ local function opts(desc)
 end
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts('next'))
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts('previous'))
-vim.keymap.set('n', '<leader>de', vim.diagnostic.open_float, opts('open float'))
+vim.keymap.set('n', '<leader>df', vim.diagnostic.open_float, opts('open float'))
 vim.keymap.set('n', '<leader>dd', '<cmd>Trouble document_diagnostics<cr>', opts('document list'))
 vim.keymap.set('n', '<leader>dw', '<cmd>Trouble workspace_diagnostics<cr>', opts('workspace list'))
 
 -- icons in gutter
 local diagnostic_signs = {
-  Error = ' ',
-  Warn = ' ',
-  Info = ' ',
-  Hint = ' ',
+  Error = '',
+  Warn = '',
+  Info = '',
+  Hint = '',
 }
 
 for type, icon in pairs(diagnostic_signs) do
