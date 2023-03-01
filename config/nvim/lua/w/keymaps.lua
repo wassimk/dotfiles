e7ad1fd83fbd8edd3719a66f9e8d8dc0fe990839
Wassim Metallaoui <wassim@metallaoui.com>
2023-02-28 19:18:35 -0600

    chore: drop iron.nvim, toggleterm can do it too

--- config/nvim/lua/w/keymaps.lua
+++ config/nvim/lua/w/keymaps.lua
@@ -30,24 +30,8 @@ vim.keymap.set('n', 't<C-a>', '<cmd>TestSuite<cr>', opts('VIM-TEST', 'suite'))
 vim.keymap.set('n', 't<C-l>', '<cmd>TestLast<cr>', opts('VIM-TEST', 'last'))
 vim.keymap.set('n', 't<C-g>', '<cmd>TestVisit<cr>', opts('VIM-TEST', 'visit'))
 
--- iron
-vim.keymap.set('n', '<Leader>pl', "<cmd>lua require('iron.core').send_line()<cr>", opts('IRON', 'send line to REPL'))
-vim.keymap.set(
-  'v',
-  '<Leader>pv',
-  "<cmd>lua require('iron.core').visual_send()<cr>",
-  opts('IRON', 'send line (visual) to REPL')
-)
-vim.keymap.set('n', '<Leader>pf', "<cmd>lua require('iron.core').send_file()<cr>", opts('IRON', 'send file to REPL'))
-vim.keymap.set('n', '<Leader>pc', "<cmd>lua require('iron.core').close_repl()<cr>", opts('IRON', 'close REPL'))
-vim.keymap.set('n', '<Leader>pr', "<cmd>lua require('iron.core').repl_restart()<cr>", opts('IRON', 'restart REPL'))
-
 -- vim-easy-align
 vim.keymap.set({ 'n', 'x' }, 'ga', '<Plug>(EasyAlign)<cr>', opts('VIM-EASY-ALIGN', 'align'))
 
 -- trouble
 vim.keymap.set('n', '<leader>t', '<cmd>TroubleToggle<cr>', opts('TROUBLE', 'toggle window'))
-
--- apple = red
--- grass += green
--- sky -= blue
