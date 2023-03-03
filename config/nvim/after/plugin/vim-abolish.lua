781d64f50423a79d252e84eed5bde7433b152f66
Wassim Metallaoui <wassim@metallaoui.com>
2023-03-03 16:22:39 -0600

    feat: abolish the words beacuse and teste

--- config/nvim/after/plugin/vim-abolish.lua
+++ config/nvim/after/plugin/vim-abolish.lua
@@ -7,4 +7,6 @@ vim.cmd([[
   Abolish precense presence
   Abolish {despa,sepe}rat{e,es,ed,ing,ely,ion,ions,or}  {despe,sepa}rat{}
   Abolish nofity notify
+  Abolish beacuse because
+  Abolish teste test
 ]])
