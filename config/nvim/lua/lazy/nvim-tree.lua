db0eb7541275509df51e5f0de078a90df39c87d4
Wassim Metallaoui <wassim@metallaoui.com>
2023-03-05 19:44:23 -0600

    fix: don't map in nvim-tree
    They switched to an on_attach mechanism but all the defaults are fine
    for me. So let's just drop this ?. By default it's g?.

--- config/nvim/lua/lazy/nvim-tree.lua
+++ config/nvim/lua/lazy/nvim-tree.lua
@@ -9,11 +9,6 @@ function M.setup()
   require('nvim-tree').setup({
     view = {
       hide_root_folder = false,
-      mappings = {
-        list = {
-          { key = '?', action = 'toggle_help' },
-        },
-      },
     },
   })
 end
