88239edabcf549005dfd6910ee4d628a06a48d1c
Wassim Metallaoui <wassim@metallaoui.com>
2023-03-10 07:04:13 -0600

    feat: add code action light bulb

---
+++ config/nvim/after/plugin/lightbulb.lua
@@ -0,0 +1,23 @@
+--
+-- nvim-lightbulb
+-- https://github.com/kosayoda/nvim-lightbulb
+--
+
+local has_lightbulb, lightbulb = pcall(require, 'nvim-lightbulb')
+
+if not has_lightbulb then
+  return
+end
+
+lightbulb.setup({
+  ignore = { 'null-ls' },
+  sign = {
+    enabled = true,
+    priority = 20,
+  },
+  autocmd = {
+    enabled = true,
+    pattern = { '*' },
+    events = { 'CursorHold', 'CursorHoldI' },
+  },
+})
