0f5bdda859113dd468ebeeda6b3b738a1df66314
Wassim Metallaoui <wassim@metallaoui.com>
2023-03-22 09:34:12 -0500

    feat: only use prettierd via null-ls if a config exists

--- config/nvim/after/plugin/null-ls.lua
+++ config/nvim/after/plugin/null-ls.lua
@@ -11,12 +11,15 @@ local sources = {
   null_ls.builtins.diagnostics.commitlint.with({
     extra_args = { '--config', os.getenv('HOME') .. '/.commitlintrc.js' },
   }),
-  null_ls.builtins.formatting.prettierd,
   null_ls.builtins.code_actions.refactoring.with({ extra_filetypes = { 'ruby' } }),
   null_ls.builtins.code_actions.shellcheck,
   null_ls.builtins.formatting.stylua,
   require('typescript.extensions.null-ls.code-actions'),
 }
+
+if utils.config_exists('.prettierrc') then
+  vim.list_extend(sources, { null_ls.builtins.formatting.prettierd })
+end
 
 if utils.config_exists('.luacheckrc') then
   vim.list_extend(sources, { null_ls.builtins.diagnostics.luacheck })
