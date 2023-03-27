d54f22ee7c44153f5eb1c51c83c16d65d5dbb520
Wassim Metallaoui <wassim@metallaoui.com>
2023-03-27 08:24:12 -0500

    feat: add erb-lint and erb-formatter via null-ls

--- config/nvim/after/plugin/null-ls.lua
+++ config/nvim/after/plugin/null-ls.lua
@@ -32,6 +32,29 @@ if utils.config_exists('stylua.toml') then
   vim.list_extend(sources, { null_ls.builtins.formatting.stylua })
 end
 
+if utils.installed_via_bundler('erb%-formatter') then
+  local formatting_source = null_ls.builtins.formatting.erb_format.with({
+    command = 'bundle',
+    args = vim.list_extend({ 'exec', 'erb-format' }, null_ls.builtins.formatting.erb_format._opts.args),
+  })
+
+  vim.list_extend(sources, { formatting_source })
+end
+
+if utils.config_exists('.erb-lint.yml') then
+  local diagnostic_source = null_ls.builtins.diagnostics.erb_lint.with({
+    command = 'bundle',
+    args = vim.list_extend({ 'exec', 'erblint' }, null_ls.builtins.diagnostics.erb_lint._opts.args),
+  })
+
+  local formatting_source = null_ls.builtins.formatting.erb_lint.with({
+    command = 'bundle',
+    args = vim.list_extend({ 'exec', 'erblint' }, null_ls.builtins.formatting.erb_lint._opts.args),
+  })
+
+  vim.list_extend(sources, { diagnostic_source, formatting_source })
+end
+
 -- rubocop via null-ls if not using solargraph or ruby-lsp
 if
   (not utils.installed_via_bundler('solargraph') and not utils.installed_via_bundler('ruby%-lsp'))
