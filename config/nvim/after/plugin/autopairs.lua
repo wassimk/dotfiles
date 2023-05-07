ceed7f8773552ca0b08bafa2359c52caa1aeb2bd
Wassim Metallaoui <wassim@metallaoui.com>
2023-05-07 11:22:15 -0500

    feat: extract nvim-autopairs to its own plugin and add cmp support

---
+++ config/nvim/after/plugin/autopairs.lua
@@ -0,0 +1,15 @@
+--
+-- nvim-autopairs
+-- https://github.com/windwp/nvim-autopairs
+--
+
+local npairs = require('nvim-autopairs')
+
+npairs.setup({
+  check_ts = true,
+  map_cr = true,
+})
+
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+local cmp = require('cmp')
+cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
