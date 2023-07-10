2f6868b08d7d743c688a93a32abbb55b8b45c9d2
Wassim Metallaoui <wassim@metallaoui.com>
2023-07-10 16:46:24 -0500

    fix: use different emmet language server
    This one better mimics vscode with a lot less auto-complete noise.

--- config/nvim/lua/w/lsp/emmet.lua
+++ config/nvim/lua/w/lsp/emmet.lua
@@ -1,16 +1,9 @@
 --
 -- emmet language server
+-- https://github.com/olrtg/emmet-language-server
 --
 
-require('lspconfig').emmet_ls.setup({
-  init_options = {
-    html = {
-      options = {
-        -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
-        ['bem.enabled'] = true,
-      },
-    },
-  },
+require('lspconfig').emmet_language_server.setup({
   capabilities = require('w.lsp').capabilities(),
   on_attach = require('w.lsp').on_attach,
 })
