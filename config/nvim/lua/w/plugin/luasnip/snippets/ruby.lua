4420111f869cd1f0641b5ef6a18750149d558637
Wassim Metallaoui <wassim@metallaoui.com>
2023-09-03 13:53:43 -0500

    feat: parse stripe snippet and grab the uri path as a choice
    A bit of a message but it works, might refactor later. Probably not!

--- config/nvim/lua/w/plugin/luasnip/snippets/ruby.lua
+++ config/nvim/lua/w/plugin/luasnip/snippets/ruby.lua
@@ -53,6 +53,46 @@ local cassette_choices = function()
   end
 end
 
+local cassette_paths = function(cassette_name)
+  local filename = cassettes_dir() .. '/stripe-' .. cassette_name .. '.yml'
+
+  local cassette = io.open(filename, 'r')
+
+  if not cassette then
+    vim.notify('Cannot open file: ' .. filename, vim.log.levels.ERROR)
+    return
+  end
+
+  local paths = {}
+  for line in cassette:lines() do
+    local path = line:match('uri: (.+)')
+
+    if path ~= nil and not vim.tbl_contains(paths, path) then
+      table.insert(paths, path)
+    end
+  end
+
+  cassette:close()
+
+  table.sort(paths)
+
+  return paths
+end
+
+local cassette_path_choices = function(cassette_name)
+  local path_choices = {}
+
+  for _, path in pairs(cassette_paths(cassette_name) or {}) do
+    if v == path then
+      return
+    else
+      table.insert(path_choices, t(path))
+    end
+  end
+
+  return c(1, path_choices)
+end
+
 return {
   s(
     {
@@ -87,7 +127,9 @@ return {
         c(1, cassette_choices()),
         i(2, 'stripe_object_name'),
         c(3, { t('get'), t('post'), t('put'), t('delete') }),
-        i(4, 'request-path'),
+        d(4, function(args)
+          return sn(nil, { cassette_path_choices(args[1][1]) })
+        end, { 1 }),
         i(0),
       }
     )
