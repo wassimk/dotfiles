d0cc544b82e0e4290b62b924739579eec2a81622
Wassim Metallaoui <wassim@metallaoui.com>
2023-09-09 18:53:03 -0500

    feat: if possible show relative to project root path in winbar

--- config/nvim/lua/w/winbar.lua
+++ config/nvim/lua/w/winbar.lua
@@ -14,12 +14,25 @@ local winbar_filetype_exclude = {
   'startuptime',
 }
 
-function M.statusline()
+local root_path = function()
+  local root_dir = vim.fs.dirname(vim.fs.find({ '.git' }, { upward = true })[1]) or ''
+  if root_dir == '' then
+    return ''
+  else
+    local relative_path = vim.fn.fnamemodify(root_dir .. '/', ':~')
+
+    return relative_path
+  end
+end
+
+function M.statusline(root_path)
   if vim.tbl_contains(winbar_filetype_exclude, vim.bo.filetype) then
     return ''
   end
 
   local file_path = vim.api.nvim_eval_statusline('%f', {}).str
+  file_path = string.gsub(file_path, root_path, '')
+
   local modified = vim.api.nvim_eval_statusline('%M', {}).str == '+' and ' ●' or ''
 
   -- file_path = file_path:gsub('/', ' > ')
@@ -31,7 +44,7 @@ function M.setup()
   vim.api.nvim_set_hl(0, 'WinBarPath', { bg = '#282c34' })
   vim.api.nvim_set_hl(0, 'WinBarModified', { fg = '#8EBD6B', bg = '#282c34' })
 
-  vim.opt.winbar = "%{%v:lua.require'w.winbar'.statusline()%}"
+  vim.opt.winbar = "%{%v:lua.require'w.winbar'.statusline('" .. root_path() .. "')%}"
 end
 
 return M
