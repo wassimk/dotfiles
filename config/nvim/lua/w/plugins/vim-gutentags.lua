f2cfad5ea12a50935c7929d072c8c1cb5d764188
Wassim Metallaoui <wassim@metallaoui.com>
2023-09-09 11:41:08 -0500

    fix: only use ruby ctag binary for ruby files

--- config/nvim/lua/w/plugins/vim-gutentags.lua
+++ config/nvim/lua/w/plugins/vim-gutentags.lua
@@ -5,8 +5,4 @@
 
 return {
   'ludovicchabant/vim-gutentags',
-  config = function()
-    vim.g.gutentags_ctags_executable_ruby = vim.fn.stdpath('data') .. '/mason/bin/ripper-tags'
-    vim.g.gutentags_ctags_extra_args = { '--ignore-unsupported-options', '--recursive' }
-  end,
 }
