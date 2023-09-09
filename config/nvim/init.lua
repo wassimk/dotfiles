deaac75738d777cbbbd319e6172362086ecb8581
Wassim Metallaoui <wassim@metallaoui.com>
2023-09-09 16:41:44 -0500

    chore: drop impatient.nvim for lazy profiling

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -2,8 +2,6 @@
 -- init.lua
 --
 
-pcall(require, 'impatient')
-
 -- remap leader and local leader to <Space>
 -- here because lazy.nvim wants this to happen first
 vim.api.nvim_set_keymap('', '<Space>', '<Nop>', {})
