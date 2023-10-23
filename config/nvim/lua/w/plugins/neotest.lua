4c5c0a7734d30eecd4caabeef68d3e8bde498c03
Wassim Metallaoui <wassim@metallaoui.com>
2023-10-23 07:25:23 -0500

    feat: add plenary to neotest

--- config/nvim/lua/w/plugins/neotest.lua
+++ config/nvim/lua/w/plugins/neotest.lua
@@ -4,6 +4,7 @@
 -- https://github.com/olimorris/neotest-rspec
 -- https://github.com/zidhuss/neotest-minitest
 -- https://github.com/nvim-neotest/neotest-jest
+-- https://github.com/nvim-neotest/neotest-plenary
 --
 
 return {
@@ -14,6 +15,7 @@ return {
     'olimorris/neotest-rspec',
     { 'zidhuss/neotest-minitest', dev = true },
     'nvim-neotest/neotest-jest',
+    'nvim-neotest/neotest-plenary',
   },
   config = function()
     require('neotest').setup({
@@ -23,6 +25,7 @@ return {
         require('neotest-jest')({
           jestCommand = 'yarn test',
         }),
+        require('neotest-plenary'),
       },
     })
   end,
