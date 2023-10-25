79bcf9fbdee4c8a8da4c86ec8191a141a5eab462
Wassim Metallaoui <wassim@metallaoui.com>
2023-10-25 08:44:09 -0500

    fix: slight improvements to neovim install script

--- bin/install-neovim.sh
+++ bin/install-neovim.sh
@@ -2,19 +2,27 @@
 
 version=${1:-stable}
 
-if [ "$version" == "nightly" ] || [ "$version" == "stable" ]; then
-  cd /tmp || exit
-  rm -f ./nvim-macos.tar.gz
-  rm -rf ./nvim-macos/
-  gh release download "$version" --pattern nvim-macos.tar.gz --repo neovim/neovim
-  xattr -c ./nvim-macos.tar.gz # avoid macOS not developer signed warnings
-  tar xzf ./nvim-macos.tar.gz
-  cd /usr/local/ || exit
-  sudo rm -rf ./nvim-macos
-  sudo mv /tmp/nvim-macos /usr/local/nvim-macos
-  ln -sf /usr/local/nvim-macos/bin/nvim /usr/local/bin/nvim
-  nvim -v
-else
+if ! [ "$version" == "nightly" ] && ! [ "$version" == "stable" ]; then
   echo "Must pass 'stable' or 'nightly' for which version to install!"
   exit 1
+fi
+
+old_version=$(nvim -v | head -n 1) 
+
+cd /tmp || exit
+gh release download "$version" --pattern nvim-macos.tar.gz --repo neovim/neovim
+xattr -c ./nvim-macos.tar.gz # avoid macOS not developer signed warnings
+tar xzf ./nvim-macos.tar.gz
+rm -f ./nvim-macos.tar.gz
+cd /usr/local/ || exit
+sudo rm -rf ./nvim-macos
+sudo mv /tmp/nvim-macos /usr/local/nvim-macos
+ln -sf /usr/local/nvim-macos/bin/nvim /usr/local/bin/nvim
+
+new_version=$(nvim -v | head -n 1) 
+
+if [ "$old_version" != "$new_version" ]; then
+  echo "Updated Neovim from $old_version to $new_version."
+else
+  echo "No change in Neovim version. Still running $old_version."
 fi
