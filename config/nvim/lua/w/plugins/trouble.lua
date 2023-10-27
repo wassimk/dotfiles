00b9c45dd2de68b2ec276e057ded5b2e0be7bb08
Wassim Metallaoui <wassim@metallaoui.com>
2023-10-27 13:39:05 -0500

    feat: lazy load trouble

--- config/nvim/lua/w/plugins/trouble.lua
+++ config/nvim/lua/w/plugins/trouble.lua
@@ -6,6 +6,9 @@
 return {
   'folke/trouble.nvim',
   cmd = { 'Trouble', 'TroubleToggle' },
+  keys = {
+    { '<leader>tr', '<cmd>TroubleToggle<cr>', mode = 'n', desc = 'TROUBLE: toggle window' },
+  },
   opts = {
     padding = false,
   },
