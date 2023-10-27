606a5fc0d513578361dd98d39e79a4f2dd8de166
Wassim Metallaoui <wassim@metallaoui.com>
2023-10-27 14:42:20 -0500

    fix: disable automatic formatting
    If it ignored markdown code blocks it would be useful!

--- config/nvim/after/ftplugin/gitcommit.lua
+++ config/nvim/after/ftplugin/gitcommit.lua
@@ -3,7 +3,7 @@
 --
 
 vim.o.spell = true
-vim.opt_local.formatoptions:append('a')
+-- vim.opt_local.formatoptions:append('a')
 
 require('w.custom.cmp.git_handles').setup()
 require('cmp_git').setup()
