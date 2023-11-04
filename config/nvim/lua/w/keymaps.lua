6492339dbdb20ec8775df47557f552a14c4079ba
Wassim Metallaoui <wassim@metallaoui.com>
2023-11-03 22:19:31 -0500

    feats: add esc keymap to quit terminals

--- config/nvim/lua/w/keymaps.lua
+++ config/nvim/lua/w/keymaps.lua
@@ -11,6 +11,9 @@ end
 -- clipboard
 vim.keymap.set({ 'n', 'v' }, '<Leader>y', '"+y', opts('CLIPBOARD', 'copy to system'))
 
+-- terminal
+vim.keymap.set('t', '<Esc>', '<Esc><C-\\><C-n><C-w>q', { desc = 'Quit and close terminal' })
+
 -- quickfix
 vim.keymap.set('n', '<leader>q', require('w.utils').toggle_qf, opts('QUICKFIX', 'toggle'))
 vim.keymap.set('n', '<leader>l', require('w.utils').toggle_loclist, opts('LOCLIST', 'toggle'))
