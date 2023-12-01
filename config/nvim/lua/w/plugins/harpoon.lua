1f808d37ebb207ade59545f8d00da97800165ebf
Wassim Metallaoui <wassim@metallaoui.com>
2023-12-01 17:25:13 -0600

    feat: use harpoon2

--- config/nvim/lua/w/plugins/harpoon.lua
+++ config/nvim/lua/w/plugins/harpoon.lua
@@ -5,46 +5,50 @@
 
 return {
   'ThePrimeagen/harpoon',
+  branch = 'harpoon2',
+  config = true,
   keys = {
     {
       '<leader>hh',
       function()
-        require('harpoon.mark').add_file()
+        require('harpoon'):list():append()
       end,
       desc = 'HARPOON: mark',
     },
     {
       '<c-e>',
       function()
-        require('harpoon.ui').toggle_quick_menu()
+        local harpoon = require('harpoon')
+
+        harpoon.ui:toggle_quick_menu(harpoon:list())
       end,
       desc = 'HARPOON: toggle UI',
     },
     {
       '<M-r>1',
       function()
-        require('harpoon.ui').nav_file(1)
+        require('harpoon'):list():select(1)
       end,
       desc = 'HARPOON: go to file 1',
     },
     {
       '<M-r>2',
       function()
-        require('harpoon.ui').nav_file(2)
+        require('harpoon'):list():select(2)
       end,
       desc = 'HARPOON: go to file 2',
     },
     {
       '<M-r>3',
       function()
-        require('harpoon.ui').nav_file(3)
+        require('harpoon'):list():select(3)
       end,
       desc = 'HARPOON: go to file 3',
     },
     {
       '<M-r>4',
       function()
-        require('harpoon.ui').nav_file(4)
+        require('harpoon'):list():select(4)
       end,
       desc = 'HARPOON: go to file 4',
     },
