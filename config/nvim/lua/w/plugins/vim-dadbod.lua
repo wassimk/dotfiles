14a6d35371a8ec7e661fe3d47d05aae87038c8be
Wassim Metallaoui <wassim@metallaoui.com>
2024-05-14 21:53:20 -0500

    feat: add plugin for sql

---
+++ config/nvim/lua/w/plugins/vim-dadbod.lua
@@ -0,0 +1,10 @@
+--
+-- vim-dadbod, vim-dadbod-ui
+-- https://github.com/tpope/vim-dadbod
+-- https://github.com/kristijanhusak/vim-dadbod-ui
+--
+
+return {
+  'tpope/vim-dadbod',
+  'kristijanhusak/vim-dadbod-ui',
+}
