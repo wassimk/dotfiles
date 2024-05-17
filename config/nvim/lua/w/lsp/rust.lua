56bc836a40c2e541ec38462ae9f819c45aa8f988
Wassim Metallaoui <wassim@metallaoui.com>
2024-05-17 12:46:40 -0500

    feat: switch to rustaceanvim for rust lsp

--- config/nvim/lua/w/lsp/rust.lua
+++ config/nvim/lua/w/lsp/rust.lua
@@ -1,22 +1,12 @@
 --
--- rust dap & language server
+-- rust language server
 --
 
-local mason_registry = require('mason-registry')
-local codelldb = mason_registry.get_package('codelldb')
-local extension_path = codelldb:get_install_path() .. '/extension/'
-local codelldb_path = extension_path .. 'adapter/codelldb'
-local liblldb_path = extension_path .. 'lldb/lib/liblldb.dylib'
-
-local opts = {
-  dap = {
-    adapter = require('rust-tools.dap').get_codelldb_adapter(codelldb_path, liblldb_path),
-  },
-  server = {
-    capabilities = require('w.lsp').capabilities(),
-    on_attach = require('w.lsp').on_attach,
-  },
-}
-
--- this plugin calls lspconfig and sets up rust-analyzer and nvim-dap
-require('rust-tools').setup(opts)
+-- setup handled by the mrcjkb/rustaceanvim plugin
+vim.g.rustaceanvim = function()
+  return {
+    server = {
+      on_attach = require('w.lsp').on_attach,
+    },
+  }
+end
---
+++ config/nvim/lua/w/plugins/rustaceanvim.lua
@@ -0,0 +1,10 @@
+--
+-- rustaceanvim
+-- https://github.com/mrcjkb/rustaceanvim
+--
+
+return {
+  'mrcjkb/rustaceanvim',
+  version = '^4',
+  lazy = false,
+}
