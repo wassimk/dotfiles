256fde8fda0736a23d41023846211929a97fbf8d
Wassim Metallaoui <wassim@metallaoui.com>
2024-05-19 15:39:18 -0500

    feat: present choices when more than one PR result

--- config/nvim/lua/w/usercmds.lua
+++ config/nvim/lua/w/usercmds.lua
@@ -15,27 +15,39 @@ vim.api.nvim_create_user_command('OpenInGHPR', function(command)
     arg = vim.fn.expand('<cword>')
   end
 
-  local url
-
   if tonumber(arg) then
     local gh_cmd = 'gh pr view ' .. arg .. ' --json url'
     local result = vim.fn.system(gh_cmd)
 
     if not string.find(result, 'Could not resolve') then
-      url = vim.json.decode(result).url
+      vim.ui.open(vim.json.decode(result).url)
+    else
+      vim.notify('PR #' .. arg .. ' not found', vim.log.INFO, { title = ':OpenInGHPR' })
     end
   else
-    local gh_cmd = 'gh pr list --search "' .. arg .. '" --state merged --json url'
-    local result = vim.json.decode(vim.fn.system(gh_cmd))
+    local gh_cmd = 'gh pr list --search "' .. arg .. '" --state merged --json number,title,author,url'
+    local results = vim.json.decode(vim.fn.system(gh_cmd))
 
-    if not vim.tbl_isempty(result) then
-      url = result[1].url
+    if vim.tbl_count(results) == 1 then
+      vim.ui.open(results[1].url)
+    elseif vim.tbl_count(results) > 1 then
+      vim.ui.select(results, {
+        prompt = 'Select a PR:',
+        format_item = function(pr)
+          local author = ''
+          if pr.author.name then
+            author = pr.author.name
+          else
+            author = 'Bot'
+          end
+
+          return '#' .. tostring(pr.number) .. ' ' .. pr.title .. ' - ' .. author
+        end,
+      }, function(choice)
+        vim.ui.open(choice.url)
+      end)
+    else
+      vim.notify('No PR found for: ' .. arg, vim.log.INFO, { title = ':OpenInGHPR' })
     end
-  end
-
-  if url == nil then
-    vim.notify('No PR found for: ' .. arg, vim.log.INFO, { title = ':OpenInGHPR' })
-  else
-    vim.ui.open(url)
   end
 end, { nargs = 1, desc = 'Open PR in browser with number, commit sha or search terms' })
