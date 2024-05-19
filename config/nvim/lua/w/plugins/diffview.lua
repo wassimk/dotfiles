d473979671f99ad11f6aee6b16e415cfe46fc839
Wassim Metallaoui <wassim@metallaoui.com>
2024-05-19 10:42:13 -0500

    feat: use q to quit the diff view

--- config/nvim/lua/w/plugins/diffview.lua
+++ config/nvim/lua/w/plugins/diffview.lua
@@ -5,13 +5,16 @@
 
 return {
   'sindrets/diffview.nvim',
-  cmd = { 'DiffviewFileHistory' },
+  cmd = { 'DiffviewFileHistory', 'DiffviewOpen' },
   config = function()
     require('diffview').setup({
       keymaps = {
         file_history_panel = {
           { 'n', 'l', require('diffview.actions').open_commit_log, { desc = 'Open the commit log panel.' } },
           { 'n', 'q', '<cmd>DiffviewClose<CR>', { desc = 'Close the commit panel and diff views.' } },
+        },
+        file_panel = {
+          { 'n', 'q', '<cmd>DiffviewClose<CR>', { desc = 'Close the file panel and diff views.' } },
         },
       },
     })
