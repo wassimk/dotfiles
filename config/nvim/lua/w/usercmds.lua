f83ac6853b634754037c3dd29cb631bc34ed4a54
Wassim Metallaoui <wassim@metallaoui.com>
2024-05-19 13:38:26 -0500

    refactor: simplify OpenInGH

--- config/nvim/lua/w/usercmds.lua
+++ config/nvim/lua/w/usercmds.lua
@@ -18,14 +18,14 @@ vim.api.nvim_create_user_command('OpenInGHPR', function(command)
   local gh_cmd, url
 
   if tonumber(arg) then
-    gh_cmd = 'gh pr view ' .. arg .. ' --json number,title,url'
+    gh_cmd = 'gh pr view ' .. arg .. ' --json url'
     local result = vim.fn.system(gh_cmd)
 
-    if not string.find(result, 'GraphQL: Could not resolve') then
+    if not string.find(result, 'Could not resolve') then
       url = vim.json.decode(result).url
     end
   else
-    gh_cmd = 'gh pr list --search "' .. arg .. '" --state merged --json number,title,url'
+    gh_cmd = 'gh pr list --search "' .. arg .. '" --state merged --json url'
     local result = vim.json.decode(vim.fn.system(gh_cmd))
 
     if not vim.tbl_isempty(result) then
@@ -34,8 +34,8 @@ vim.api.nvim_create_user_command('OpenInGHPR', function(command)
   end
 
   if url == nil then
-    vim.notify('No PR found for ' .. arg, vim.log.INFO, { title = ':OpenInGHPR' })
+    vim.notify('No PR found for: ' .. arg, vim.log.INFO, { title = ':OpenInGHPR' })
   else
     vim.ui.open(url)
   end
-end, { nargs = 1, desc = 'Open PR in browser with number or commit sha' })
+end, { nargs = 1, desc = 'Open PR in browser with number, commit sha or search terms' })
