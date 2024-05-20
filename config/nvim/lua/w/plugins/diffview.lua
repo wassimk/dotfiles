df1c56c6406f8a6aea0f90a035f2ffea050ffadd
Wassim Metallaoui <wassim@metallaoui.com>
2024-05-20 07:34:09 -0500

    feat: customize diffview.nvim keymaps and default switches
    These all show up in the help panel showing the keymaps so this mostly
    removes duplicates and changes some to my own muscle memory from ther
    tools.

--- config/nvim/lua/w/plugins/diffview.lua
+++ config/nvim/lua/w/plugins/diffview.lua
@@ -8,13 +8,58 @@ return {
   cmd = { 'DiffviewFileHistory', 'DiffviewOpen' },
   config = function()
     require('diffview').setup({
+      enhanced_diff_hl = true,
+      default_args = {
+        DiffviewFileHistory = { '--no-merges' },
+      },
       keymaps = {
         file_history_panel = {
-          { 'n', 'l', require('diffview.actions').open_commit_log, { desc = 'Open the commit log panel.' } },
-          { 'n', 'q', '<cmd>DiffviewClose<CR>', { desc = 'Close the commit panel and diff views.' } },
+
+          { 'n', 'l', require('diffview.actions').open_commit_log, { desc = 'Open the commit log panel' } },
+          { 'n', 'q', '<cmd>DiffviewClose<CR>', { desc = 'Close the file history panel and diff views' } },
+          {
+            'n',
+            '<C-v>',
+            require('diffview.actions').goto_file_split,
+            { desc = 'Open the file in a new split' },
+          },
+          {
+            'n',
+            '<C-t>',
+            require('diffview.actions').goto_file_tab,
+            { desc = 'Open the file in a new tabpage' },
+          },
+          ['<down>'] = false, -- use `j`
+          ['<up>'] = false, -- use `k`
+          ['L'] = false, -- use `l`
+          ['<cr>'] = false, -- use `o`
+          ['<C-w><C-f>'] = false, -- use `<C-v>`
+          ['<C-w>gf'] = false, -- use `<C-t>`
+          ['<2-LeftMouse>'] = false, -- use keyboard
         },
         file_panel = {
-          { 'n', 'q', '<cmd>DiffviewClose<CR>', { desc = 'Close the file panel and diff views.' } },
+          { 'n', 'l', require('diffview.actions').open_commit_log, { desc = 'Open the commit log panel' } },
+          { 'n', 'q', '<cmd>DiffviewClose<CR>', { desc = 'Close the file panel and diff views' } },
+          {
+            'n',
+            '<C-v>',
+            require('diffview.actions').goto_file_split,
+            { desc = 'Open the file in a new split' },
+          },
+          {
+            'n',
+            '<C-t>',
+            require('diffview.actions').goto_file_tab,
+            { desc = 'Open the file in a new tabpage' },
+          },
+          ['<down>'] = false, -- use `j`
+          ['<up>'] = false, -- use `k`
+          ['L'] = false, -- use `o`
+          ['<cr>'] = false, -- use `o`
+          ['<C-w><C-f>'] = false, -- use `<C-v>`
+          ['<C-w>gf'] = false, -- use `<C-t>`
+          ['<2-LeftMouse>'] = false, -- use keyboard
+          ['-'] = false, -- use `s`
         },
       },
     })
