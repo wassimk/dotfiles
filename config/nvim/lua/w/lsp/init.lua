0e656108b8cd1b693578aad2756ebd8189f6b49a
Wassim Metallaoui <wassim@metallaoui.com>
2024-05-21 07:46:25 -0500

    refactor: put ruby lsp commands inside its own lsp config

--- config/nvim/lua/w/lsp/init.lua
+++ config/nvim/lua/w/lsp/init.lua
@@ -4,67 +4,6 @@
 
 local M = {}
 
-local function add_ruby_deps_command(client, bufnr)
-  vim.api.nvim_buf_create_user_command(bufnr, 'RubyLSPShowDependencies', function(opts)
-    local params = vim.lsp.util.make_text_document_params()
-    local showAll = opts.args == 'all'
-
-    client.request('rubyLsp/workspace/dependencies', params, function(error, result)
-      if error then
-        print('Error showing Ruby dependencies:')
-        print(vim.inspect(error))
-        return
-      end
-
-      local qf_list = {}
-      for _, item in ipairs(result) do
-        if showAll or item.dependency then
-          table.insert(qf_list, {
-            text = string.format('%s (%s) - %s', item.name, item.version, item.dependency),
-            filename = item.path,
-          })
-        end
-      end
-
-      vim.fn.setqflist(qf_list)
-      vim.cmd('copen')
-    end, bufnr)
-  end, {
-    nargs = '?',
-    desc = 'Show Ruby dependencies for current workspace',
-    complete = function()
-      return { 'all' }
-    end,
-  })
-end
-
-local function add_ruby_syntax_tree_command(client, bufnr)
-  vim.api.nvim_buf_create_user_command(bufnr, 'RubyLSPShowSyntaxTree', function()
-    local params = vim.lsp.util.make_position_params()
-
-    client.request('rubyLsp/textDocument/showSyntaxTree', params, function(error, result)
-      if error then
-        print('Error showing Syntax Tree for buffer: ')
-        print(vim.inspect(error))
-        return
-      end
-
-      local ast = result.ast
-      local lines = {}
-      for line in ast:gmatch('[^\r\n]+') do
-        table.insert(lines, line)
-      end
-
-      vim.api.nvim_command('vnew')
-      local buf = vim.api.nvim_get_current_buf()
-      vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
-    end, bufnr)
-  end, {
-    nargs = '?',
-    desc = 'Show Ruby Syntax Tree for current buffer',
-  })
-end
-
 function M.setup()
   -- change border for lsp floats to single
   local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
@@ -95,7 +34,7 @@ function M.capabilities()
   return require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 end
 
-function M.on_attach(client, bufnr)
+function M.on_attach(client)
   local function opts(desc)
     return {
       buffer = 0,
@@ -140,11 +79,6 @@ function M.on_attach(client, bufnr)
     if client.name == 'tsserver' then -- need newer typescript in my projects
       vim.lsp.inlay_hint.disable()
     end
-  end
-
-  if client.name == 'ruby_lsp' then
-    add_ruby_deps_command(client, bufnr)
-    add_ruby_syntax_tree_command(client, bufnr)
   end
 end
 
