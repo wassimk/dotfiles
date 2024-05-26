7726e4a5437d11f9e2121e18b21559279afc6176
Wassim Metallaoui <wassim@metallaoui.com>
2024-05-26 16:29:02 -0500

    feat: replace openingh.nvim with gh-navigator.nvim

---
+++ config/nvim/lua/w/plugins/gh-navigator.lua
@@ -0,0 +1,11 @@
+--
+-- gh-navigator
+-- https://github.com/wassimk/gh-navigator.nvim
+--
+
+return {
+  'wassimk/gh-navigator.nvim',
+  cmd = { 'GHPR', 'GHBlameFile', 'GHBlameLines', 'GHFile', 'GHFileLines', 'GHRepo' },
+  config = true,
+  dev = true,
+}
