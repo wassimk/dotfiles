d91a184bde7088269ee5757a604016ed734b2627
Wassim Metallaoui <wassim@metallaoui.com>
2024-05-27 08:45:13 -0500

    feat: extract the scalpel tool to a plugin

--- config/nvim/lua/w/keymaps.lua
+++ config/nvim/lua/w/keymaps.lua
@@ -18,9 +18,6 @@ vim.keymap.set('t', '<Esc>', '<Esc><C-\\><C-n><C-w>q', { desc = 'Quit and close 
 vim.keymap.set('n', '<leader>q', require('w.utils').toggle_qf, opts('QUICKFIX', 'toggle'))
 vim.keymap.set('n', '<leader>l', require('w.utils').toggle_loclist, opts('LOCLIST', 'toggle'))
 
--- substitute current word in file
-vim.keymap.set('n', '<leader>e', require('w.utils').substitute_current_word, opts('', 'Substite current word in file'))
-
 -- execute current line or file
 vim.keymap.set('n', '<leader>x', '<cmd>.lua<CR>', opts('', 'Execute the current line'))
 vim.keymap.set('n', '<leader><leader>x', '<cmd>source %<CR>', opts('', 'Execute the current file'))
---
+++ config/nvim/lua/w/plugins/scalpel.lua
@@ -0,0 +1,10 @@
+--
+-- scalpel.nvim
+-- https://www.github.com/wassimk/scalpel.nvim
+--
+
+return {
+  'wassimk/scalpel.nvim',
+  config = true,
+  dev = true,
+}
