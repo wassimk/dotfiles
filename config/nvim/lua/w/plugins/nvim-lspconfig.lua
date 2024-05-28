9f9495e7e0156c2ebb5672134ab527abaa5bbb4c
Wassim Metallaoui <wassim@metallaoui.com>
2024-05-28 08:15:01 -0500

    fix: stop including LSP percent for Ruby LSP
    They already include the percent in the message so we see two
    percentages during progress updates. If the LSP gives the percentage
    they should update the client to stop including it in the message
    string.
    
    This is the default formatter function form the plugin with the LSP
    check lines added.

--- config/nvim/lua/w/plugins/nvim-lspconfig.lua
+++ config/nvim/lua/w/plugins/nvim-lspconfig.lua
@@ -3,6 +3,20 @@
 -- https://github.com/neovim/nvim-lspconfig
 --
 
+local function fidget_format_message(msg)
+  local lsp_name = msg.lsp_client.config.name
+  local message = msg.message
+
+  if not message then
+    message = msg.done and 'Completed' or 'In progress...'
+  end
+
+  if msg.percentage ~= nil and not lsp_name == 'ruby_lsp' then
+    message = string.format('%s (%.0f%%)', message, msg.percentage)
+  end
+  return message
+end
+
 return {
   'neovim/nvim-lspconfig',
   event = { 'BufNewFile', 'BufReadPre', 'CmdlineEnter' },
@@ -14,7 +28,14 @@ return {
     -- { 'antosha417/nvim-lsp-file-operations', config = true },
     {
       'j-hui/fidget.nvim',
-      config = true,
+      config = {
+        progress = {
+          display = {
+
+            format_message = fidget_format_message,
+          },
+        },
+      },
     },
   },
 }
