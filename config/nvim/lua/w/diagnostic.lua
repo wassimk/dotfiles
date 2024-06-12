e91206bfc4d1ef4cbe7e28c34ddc609817539c0b
Wassim Metallaoui <wassim@metallaoui.com>
2024-06-11 21:04:49 -0500

    feat: use new vim.diagnostic.jump functions

--- config/nvim/lua/w/diagnostic.lua
+++ config/nvim/lua/w/diagnostic.lua
@@ -45,8 +45,12 @@ function M.setup()
       desc = 'DIAGNOSTIC: ' .. desc,
     }
   end
-  vim.keymap.set('n', '<leader>dn', vim.diagnostic.goto_next, opts('jump next'))
-  vim.keymap.set('n', '<leader>dp', vim.diagnostic.goto_prev, opts('jump previous'))
+  vim.keymap.set('n', '<leader>dn', function()
+    vim.diagnostic.jump({ count = 1, float = true })
+  end, opts('jump next'))
+  vim.keymap.set('n', '<leader>dp', function()
+    vim.diagnostic.jump({ count = -1, foat = true })
+  end, opts('jump previous'))
   vim.keymap.set('n', '<leader>df', vim.diagnostic.open_float, opts('open float'))
   vim.keymap.set('n', '<leader>dd', vim.diagnostic.setloclist, opts('current document'))
   vim.keymap.set('n', '<leader>dw', vim.diagnostic.setqflist, opts('current workspace'))
