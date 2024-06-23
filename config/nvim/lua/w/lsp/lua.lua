ed0381c327b5c14f12bb69ee84e78c7d86b228bb
Wassim Metallaoui <wassim@metallaoui.com>
2024-06-23 13:06:53 -0500

    chore: address vim not being a global

--- config/nvim/lua/w/lsp/lua.lua
+++ config/nvim/lua/w/lsp/lua.lua
@@ -7,7 +7,7 @@ require('lspconfig').lua_ls.setup({
     Lua = {
       -- https://github.com/LuaLS/lua-language-server/wiki/Settings
       completion = { enable = true, showWord = 'Disable' },
-      diagnostics = { globals = { 'hs' } },
+      diagnostics = { globals = { 'vim', 'hs' } },
       hint = { enable = true, arrayIndex = 'Disable' },
       format = { enable = false },
       telemetry = { enable = false },
