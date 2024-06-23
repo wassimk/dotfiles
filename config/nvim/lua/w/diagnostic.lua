ce0f3e81658287b6ab0fd3ce9329fbda426b730f
Wassim Metallaoui <wassim@metallaoui.com>
2024-06-23 13:07:20 -0500

    feat: use the new vim.diagnostic.config for sign icons too

--- config/nvim/lua/w/diagnostic.lua
+++ config/nvim/lua/w/diagnostic.lua
@@ -29,11 +29,21 @@ function M.signs(abbreviated)
 end
 
 function M.setup()
+  -- icons in gutter
+  local signs = {}
+
+  for type, icon in pairs(M.signs(true)) do
+    signs[vim.diagnostic.severity[type]] = icon
+  end
+
   -- global configuration
   vim.diagnostic.config({
     virtual_text = false,
     underline = false,
-    signs = { priority = 10 },
+    signs = {
+      priority = 10,
+      text = signs,
+    },
     float = {
       source = 'if_many',
     },
@@ -54,13 +64,6 @@ function M.setup()
   vim.keymap.set('n', '<leader>df', vim.diagnostic.open_float, opts('open float'))
   vim.keymap.set('n', '<leader>dd', vim.diagnostic.setloclist, opts('current document'))
   vim.keymap.set('n', '<leader>dw', vim.diagnostic.setqflist, opts('current workspace'))
-
-  -- icons in gutter
-  for type, icon in pairs(M.signs()) do
-    local hl = 'DiagnosticSign' .. type
-
-    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
-  end
 end
 
 return M
