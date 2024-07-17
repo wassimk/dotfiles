c6958e8c205c550e6ea973b0f5a0b88d4d44b678
Wassim Metallaoui <wassim@metallaoui.com>
2024-07-17 08:36:25 -0500

    chore: use vim-illuminuate over the native documentHighlight
    I still prefer to use document highlight via the native configuration
    but it doesn't reliablity initiate with ruby-lsp. I'm unsure why, likely
    a timing thing with on attach.

---
+++ config/nvim/lua/w/plugins/vim-illuminate.lua
@@ -0,0 +1,17 @@
+--
+-- vim-illuminate
+-- https://github.com/rrethy/vim-illuminate
+--
+
+return {
+  'rrethy/vim-illuminate',
+  config = function()
+    require('illuminate').configure({
+      providers = {
+        'lsp',
+        -- 'treesitter',
+        -- 'regex',
+      },
+    })
+  end,
+}
