6591cf04fe6480c0cf42ba0a982b249991592f75
Wassim Metallaoui <wassim@metallaoui.com>
2024-10-11 10:37:34 -0500

    fix: nested formatter config no longer supported

--- config/nvim/lua/w/plugins/conform.lua
+++ config/nvim/lua/w/plugins/conform.lua
@@ -7,10 +7,10 @@ return {
   opts = {
     formatters_by_ft = {
       lua = { 'stylua' },
-      javascript = { { 'prettierd' } },
-      javascriptreact = { { 'prettierd' } },
-      typescript = { { 'prettierd' } },
-      typescriptreact = { { 'prettierd' } },
+      javascript = { 'prettierd' },
+      javascriptreact = { 'prettierd' },
+      typescript = { 'prettierd' },
+      typescriptreact = { 'prettierd' },
     },
     formatters = {
       prettierd = {
