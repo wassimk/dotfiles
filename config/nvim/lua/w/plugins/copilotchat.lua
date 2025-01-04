604932a199931eebdd96f85bed52b346054388b5
Wassim Metallaoui <wassim@metallaoui.com>
2025-01-04 11:45:49 -0600

    fix: correct issues with copilotchat errors
    This is mostly a copy/paste from LazyNvim repo with slight tweaks.

--- config/nvim/lua/w/plugins/copilotchat.lua
+++ config/nvim/lua/w/plugins/copilotchat.lua
@@ -20,76 +20,67 @@ function M.pick(kind)
 end
 
 return {
-  'CopilotC-Nvim/CopilotChat.nvim',
-  cmd = { 'CopilotChat', 'CopilotChatExplain', 'CopilotChatCommitStaged' },
-  dependencies = {
-    { 'zbirenbaum/copilot.lua' }, -- or github/copilot.vim
-  },
-
-  opts = function()
-    local user = vim.env.USER or 'User'
-    user = user:sub(1, 1):upper() .. user:sub(2)
-
-    return {
-      context = 'buffers',
-      answer_header = '  Copilot ',
-      question_header = '  ' .. user .. ' ',
-      chat_autocomplete = true,
-      auto_insert_mode = true,
-      selection = function(source)
-        local select = require('CopilotChat.select')
-        return select.visual(source) or select.buffer(source)
-      end,
-      window = {
-        layout = 'horizontal',
+  {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    branch = 'main',
+    cmd = 'CopilotChat',
+    opts = function()
+      local user = vim.env.USER or 'User'
+      user = user:sub(1, 1):upper() .. user:sub(2)
+      return {
+        auto_insert_mode = true,
+        question_header = '  ' .. user .. ' ',
+        answer_header = '  Copilot ',
+        window = {
+          layout = 'horizontal',
+        },
+      }
+    end,
+    keys = {
+      { '<c-s>', '<CR>', ft = 'copilot-chat', desc = 'Submit Prompt', remap = true },
+      { '<leader>a', '', desc = '+ai', mode = { 'n', 'v' } },
+      {
+        '<leader>aa',
+        function()
+          return require('CopilotChat').toggle()
+        end,
+        desc = 'COPILOT: Toggle',
+        mode = { 'n', 'v' },
       },
-    }
-  end,
-
-  config = function(_, opts)
-    local chat = require('CopilotChat')
-
-    vim.api.nvim_create_autocmd('BufEnter', {
-      pattern = 'copilot-chat',
-      callback = function()
-        vim.opt_local.relativenumber = false
-        vim.opt_local.number = false
-      end,
-    })
-
-    chat.setup(opts)
-  end,
-
-  keys = {
-    { '<leader>a', '', desc = '+ai prefix', mode = { 'n', 'v' } },
-    {
-      '<leader>aa',
-      function()
-        return require('CopilotChat').toggle()
-      end,
-      desc = 'COPILOTCHAT: Toggle',
-      mode = { 'n', 'v' },
+      {
+        '<leader>ax',
+        function()
+          return require('CopilotChat').reset()
+        end,
+        desc = 'COPILOT: clear',
+        mode = { 'n', 'v' },
+      },
+      {
+        '<leader>aq',
+        function()
+          local input = vim.fn.input('Quick Chat: ')
+          if input ~= '' then
+            require('CopilotChat').ask(input)
+          end
+        end,
+        desc = 'COPILOT: Quick Chat',
+        mode = { 'n', 'v' },
+      },
+      -- Show prompts actions with telescope
+      { '<leader>ap', M.pick('prompt'), desc = 'COPILOT: Prompt Actions', mode = { 'n', 'v' } },
     },
-    {
-      '<leader>ax',
-      function()
-        return require('CopilotChat').reset()
-      end,
-      desc = 'COPILOTCHAT: Clear/Reset',
-      mode = { 'n', 'v' },
-    },
-    {
-      '<leader>aq',
-      function()
-        local input = vim.fn.input('Quick Chat: ')
-        if input ~= '' then
-          require('CopilotChat').ask(input)
-        end
-      end,
-      desc = 'COPILOTCHAT: Quick Chat',
-      mode = { 'n', 'v' },
-    },
-    { '<leader>ad', M.pick('help'), desc = 'COPILOTCHAT: Diagnostic Help', mode = { 'n', 'v' } },
-    { '<leader>ap', M.pick('prompt'), desc = 'COPILOTCHAT: Prompt Actions', mode = { 'n', 'v' } },
+    config = function(_, opts)
+      local chat = require('CopilotChat')
+
+      vim.api.nvim_create_autocmd('BufEnter', {
+        pattern = 'copilot-chat',
+        callback = function()
+          vim.opt_local.relativenumber = false
+          vim.opt_local.number = false
+        end,
+      })
+
+      chat.setup(opts)
+    end,
   },
 }
