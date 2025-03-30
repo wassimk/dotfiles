6ab80fdbc5e8dbfa0a0b66f80f886dbbf69328c2
Wassim Metallaoui <wassim@metallaoui.com>
2025-03-30 10:59:17 -0500

    feat: move alpha-nvim config to simpler Snacks.dashboard

--- config/nvim/lua/w/plugins/snacks.lua
+++ config/nvim/lua/w/plugins/snacks.lua
@@ -7,5 +7,27 @@ return {
   'folke/snacks.nvim',
   opts = {
     lazygit = {},
+    dashboard = {
+      sections = {
+        { section = 'header' },
+        { icon = ' ', title = 'Keymaps', section = 'keys', indent = 2, padding = 1 },
+        { icon = ' ', title = 'Recent Files', section = 'recent_files', limit = 8, indent = 2, padding = 1 },
+        { icon = ' ', title = 'Projects', section = 'projects', indent = 2, padding = 1 },
+        { section = 'startup' },
+      },
+      preset = {
+        keys = {
+          { icon = '', key = 'n', desc = 'New File', action = ':ene | startinsert' },
+          { icon = '󰥩', key = '<leader>f', desc = 'Find File', action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = '󰩊', key = '<leader>s', desc = 'Find Text', action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = '󰪸 ', key = '<C-f> h', desc = 'Search help' },
+          { icon = '󰩊', key = 'r', desc = 'Recent Files', action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          { icon = '󰪸', key = 's', desc = 'Restore Session', section = 'session' },
+          { icon = '󰄧', key = 'p', desc = 'Profile', action = '<cmd>Lazy profile<cr>' },
+          { icon = '', key = 'u', desc = 'Update plugins', action = '<cmd>Lazy sync<cr>' },
+          { icon = '󱎘', key = 'q', desc = 'Quit', action = ':qa' },
+        },
+      },
+    },
   },
 }
