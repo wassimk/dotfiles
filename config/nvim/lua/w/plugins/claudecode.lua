6037ec350cb58f62a0ba2bcf8800feccc5d6c786
Wassim Metallaoui <wassim@metallaoui.com>
2025-06-22 16:50:51 -0500

    feat: switch to claudecode.nvim over claude-code.nvim

---
+++ config/nvim/lua/w/plugins/claudecode.lua
@@ -0,0 +1,29 @@
+--
+-- claudecode.nvim
+-- https://github.com/coder/claudecode.nvim
+--
+
+return {
+  'coder/claudecode.nvim',
+  dependencies = { 'folke/snacks.nvim' },
+  config = true,
+  keys = {
+    { '<leader>a', nil, desc = '+AI' },
+    { '<leader>aa', '<cmd>ClaudeCode<cr>', desc = 'Toggle Claude' },
+    { '<leader>af', '<cmd>ClaudeCodeFocus<cr>', desc = 'Focus Claude' },
+    { '<leader>ar', '<cmd>ClaudeCode --resume<cr>', desc = 'Resume Claude' },
+    { '<leader>aC', '<cmd>ClaudeCode --continue<cr>', desc = 'Continue Claude' },
+    { '<leader>ab', '<cmd>ClaudeCodeAdd %<cr>', desc = 'Add current buffer' },
+    { '<leader>as', '<cmd>ClaudeCodeSend<cr>', mode = 'v', desc = 'Send to Claude' },
+    {
+      '<leader>as',
+      '<cmd>ClaudeCodeTreeAdd<cr>',
+      desc = 'Add file',
+      ft = { 'NvimTree' },
+    },
+    -- Diff management
+    { '<leader>ag', '', mode = { 'n', 'v' }, desc = '+Diff Management' },
+    { '<leader>aga', '<cmd>ClaudeCodeDiffAccept<cr>', desc = 'Accept diff' },
+    { '<leader>agr', '<cmd>ClaudeCodeDiffDeny<cr>', desc = 'Reject diff' },
+  },
+}
