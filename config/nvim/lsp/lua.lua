fe369add0c153118ac5290cec9589cf741f97265
Wassim Metallaoui <wassim@metallaoui.com>
2025-06-26 21:33:00 -0500

    fix: attempt to fix default lua_ls configuration

--- config/nvim/lsp/lua.lua
+++ config/nvim/lsp/lua.lua
@@ -3,21 +3,26 @@
 --
 
 local default_config = require('lspconfig.configs.lua_ls').default_config
-default_config.root_dir = nil
 
 local custom_config = {
   cmd = { 'lua-language-server' },
   filetypes = { 'lua' },
-  root_markers = { '.luarc.json', '.luarc.jsonc' },
+  root_dir = require('lspconfig.util').root_pattern('.luarc.json', '.luarc.jsonc', '.git'),
   settings = {
     Lua = {
       -- https://github.com/LuaLS/lua-language-server/wiki/Settings
+      runtime = {
+        version = 'LuaJIT',
+      },
       completion = { enable = true, showWord = 'Disable' },
       diagnostics = { globals = { 'vim', 'hs' } },
       hint = { enable = true, arrayIndex = 'Disable' },
       format = { enable = false },
       telemetry = { enable = false },
-      workspace = { checkThirdParty = false },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file('', true),
+        checkThirdParty = false,
+      },
     },
   },
   capabilities = require('w.lsp').capabilities(),
