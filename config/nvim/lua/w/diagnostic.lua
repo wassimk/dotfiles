08625a72dc581fd716b349e638b462b9be772506
Wassim Metallaoui <wassim@metallaoui.com>
2025-06-26 21:32:46 -0500

    fix: no more diagnostic underlines

--- config/nvim/lua/w/diagnostic.lua
+++ config/nvim/lua/w/diagnostic.lua
@@ -41,7 +41,7 @@ function M.setup()
     virtual_text = {
       current_line = true,
     },
-    underline = true,
+    underline = false,
     signs = {
       priority = 10,
       text = signs,
