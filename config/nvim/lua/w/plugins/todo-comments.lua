bfaee5076b1bc9590dc273fc772f454707a3a902
Wassim Metallaoui <wassim@metallaoui.com>
2025-06-28 09:18:53 -0500

    feat: add a todo comments command for snacks

--- config/nvim/lua/w/plugins/todo-comments.lua
+++ config/nvim/lua/w/plugins/todo-comments.lua
@@ -6,9 +6,16 @@
 return {
   'folke/todo-comments.nvim',
   event = 'BufRead',
-  cmd = { 'TodoLocList', 'TodoQuickFix' },
+  cmd = { 'TodoLocList', 'TodoQuickFix', 'TodoSnacks' },
   opts = {
     signs = false,
     sign_priority = 5, -- lower than gitsigns
   },
+  config = function(_, opts)
+    require('todo-comments').setup(opts)
+
+    vim.api.nvim_create_user_command('TodoSnacks', function()
+      require('snacks').picker.todo_comments()
+    end, { desc = 'Find todo comments with Snacks picker' })
+  end,
 }
