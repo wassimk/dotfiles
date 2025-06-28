848fb7a43fb59cab10757ce4bdb3a51d17ffc3cb
Wassim Metallaoui <wassim@metallaoui.com>
2025-06-28 17:06:11 -0500

    fix: update rust lsp

--- config/nvim/lua/w/plugins/rustaceanvim.lua
+++ config/nvim/lua/w/plugins/rustaceanvim.lua
@@ -3,8 +3,15 @@
 -- https://github.com/mrcjkb/rustaceanvim
 --
 
+vim.g.rustaceanvim = {
+  server = {
+    capabilities = require('w.lsp').capabilities(),
+    on_attach = require('w.lsp').on_attach,
+  },
+}
+
 return {
   'mrcjkb/rustaceanvim',
-  version = '^4',
-  lazy = false,
+  version = '^6',
+  lazy = false, -- This plugin is already lazy
 }
