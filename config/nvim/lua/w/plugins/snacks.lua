9d784feb484568e9681fcf902382b62f005118d2
Wassim Metallaoui <wassim@metallaoui.com>
2025-06-28 19:08:16 -0500

    feat: add search_in_directory, diff and copy_file_path picker actions

--- config/nvim/lua/w/plugins/snacks.lua
+++ config/nvim/lua/w/plugins/snacks.lua
@@ -25,23 +25,117 @@ local explorer_opts = {
   },
 }
 
+local common_picker_keymaps = {
+  input = {
+    keys = {
+      ['<Esc>'] = { 'close', mode = { 'n', 'i' } },
+      ['<c-u>'] = { 'preview_scroll_up', mode = { 'i', 'n' } },
+      ['<c-d>'] = { 'preview_scroll_down', mode = { 'i', 'n' } },
+      ['<c-x>'] = { 'edit_split', mode = { 'i', 'n' } },
+      ['<c-l>'] = { 'search_in_directory', mode = { 'i', 'n' } },
+      ['C'] = { 'copy_file_path', mode = { 'i', 'n' } },
+      ['D'] = { 'diff', mode = { 'i', 'n' } },
+    },
+  },
+  list = {
+    keys = {
+      ['<c-u>'] = 'preview_scroll_up',
+      ['<c-d>'] = 'preview_scroll_down',
+      ['<c-x>'] = 'edit_split',
+      ['<c-l>'] = 'search_in_directory',
+      ['C'] = 'copy_file_path',
+      ['D'] = 'diff',
+    },
+  },
+}
+
+local common_picker_actions = {
+  copy_file_path = {
+    action = function(_, item)
+      if not item then
+        return
+      end
+
+      local vals = {
+        ['BASENAME'] = vim.fn.fnamemodify(item.file, ':t:r'),
+        ['EXTENSION'] = vim.fn.fnamemodify(item.file, ':t:e'),
+        ['FILENAME'] = vim.fn.fnamemodify(item.file, ':t'),
+        ['PATH'] = item.file,
+        ['PATH (CWD)'] = vim.fn.fnamemodify(item.file, ':.'),
+        ['PATH (HOME)'] = vim.fn.fnamemodify(item.file, ':~'),
+        ['URI'] = vim.uri_from_fname(item.file),
+      }
+
+      local options = vim.tbl_filter(function(val)
+        return vals[val] ~= ''
+      end, vim.tbl_keys(vals))
+      if vim.tbl_isempty(options) then
+        vim.notify('No values to copy', vim.log.levels.WARN)
+        return
+      end
+      table.sort(options)
+      vim.ui.select(options, {
+        prompt = 'Choose to copy to clipboard:',
+        format_item = function(list_item)
+          return ('%s: %s'):format(list_item, vals[list_item])
+        end,
+      }, function(choice)
+        local result = vals[choice]
+        if result then
+          vim.fn.setreg('+', result)
+          Snacks.notify.info('Yanked `' .. result .. '`')
+        end
+      end)
+    end,
+  },
+  search_in_directory = {
+    action = function(_, item)
+      if not item then
+        return
+      end
+      local dir = vim.fn.fnamemodify(item.file, ':p:h')
+      Snacks.picker.grep({
+        cwd = dir,
+        show_empty = true,
+        hidden = true,
+        ignored = true,
+        follow = false,
+        supports_live = true,
+      })
+    end,
+  },
+  diff = {
+    action = function(picker)
+      picker:close()
+      local sel = picker:selected()
+      if #sel > 0 and sel then
+        Snacks.notify.info(sel[1].file)
+        vim.cmd('tabnew ' .. sel[1].file)
+        vim.cmd('vert diffs ' .. sel[2].file)
+        Snacks.notify.info('Diffing ' .. sel[1].file .. ' against ' .. sel[2].file)
+        return
+      end
+
+      Snacks.notify.info('Select two entries for the diff')
+    end,
+  },
+}
+
+local common_picker_opts = {
+  win = common_picker_keymaps,
+  actions = common_picker_actions,
+}
+
 local picker_opts = {
-  win = {
-    input = {
-      keys = {
-        ['<Esc>'] = { 'close', mode = { 'n', 'i' } },
-        ['<c-u>'] = { 'preview_scroll_up', mode = { 'i', 'n' } },
-        ['<c-d>'] = { 'preview_scroll_down', mode = { 'i', 'n' } },
-        ['<c-x>'] = { 'edit_split', mode = { 'i', 'n' } },
-      },
-    },
-    list = {
-      keys = {
-        ['<c-u>'] = 'preview_scroll_up',
-        ['<c-d>'] = 'preview_scroll_down',
-        ['<c-x>'] = 'edit_split',
-      },
-    },
+  enabled = true,
+  ui_select = true,
+  sources = {
+    explorer = explorer_opts,
+    files = common_picker_opts,
+    buffers = common_picker_opts,
+    smart = common_picker_opts,
+    grep = common_picker_opts,
+    grep_word = common_picker_opts,
   },
 }
 
@@ -63,7 +157,7 @@ local dashboard_opts = {
         key = '<leader>f',
         desc = 'Find File',
         action = function()
-          Snacks.picker.files(picker_opts)
+          Snacks.picker.files()
         end,
       },
       {
@@ -71,7 +165,7 @@ local dashboard_opts = {
         key = '<leader>s',
         desc = 'Find Text',
         action = function()
-          Snacks.picker.grep(picker_opts)
+          Snacks.picker.grep()
         end,
       },
       { icon = '󰪸 ', key = '<C-f> h', desc = 'Search help' },
@@ -80,7 +174,7 @@ local dashboard_opts = {
         key = 'r',
         desc = 'Recent Files',
         action = function()
-          Snacks.picker.recent(picker_opts)
+          Snacks.picker.recent()
         end,
       },
       { icon = '󰪸', key = 's', desc = 'Restore Session', section = 'session' },
@@ -99,22 +193,21 @@ return {
     lazygit = {},
     image = {},
     input = {},
-    explorer = {},
-    picker = {},
+    picker = picker_opts,
     dashboard = dashboard_opts,
   },
   keys = {
     {
       '<C-n>',
       function()
-        Snacks.explorer(explorer_opts)
+        Snacks.explorer()
       end,
       desc = 'FILES: toggle explorer',
     },
     {
       '<leader>f',
       function()
-        Snacks.picker.smart(picker_opts)
+        Snacks.picker.smart()
       end,
       mode = { 'n', 'x' },
       desc = 'SNACKS: smart',
@@ -122,7 +215,7 @@ return {
     {
       '<leader>b',
       function()
-        Snacks.picker.buffers(picker_opts)
+        Snacks.picker.buffers()
       end,
       mode = { 'n', 'x' },
       desc = 'SNACKS: buffers',
@@ -130,7 +223,7 @@ return {
     {
       '<leader>s',
       function()
-        Snacks.picker.grep(picker_opts)
+        Snacks.picker.grep()
       end,
       mode = { 'n', 'x' },
       desc = 'SNACKS: grep',
@@ -138,7 +231,7 @@ return {
     {
       '<leader>w',
       function()
-        Snacks.picker.grep_word(picker_opts)
+        Snacks.picker.grep_word()
       end,
       mode = { 'n', 'x' },
       desc = 'SNACKS: grep word',
@@ -178,7 +271,7 @@ return {
     {
       '<C-f>f',
       function()
-        Snacks.picker.files(picker_opts)
+        Snacks.picker.files()
       end,
       mode = { 'n', 'x' },
       desc = 'SNACKS: files',
