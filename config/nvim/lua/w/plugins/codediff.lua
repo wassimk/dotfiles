fb609217b3b0fab8d5fd7a0391c83065d493b1da
Wassim Metallaoui <wassim@metallaoui.com>
2026-02-15 08:21:29 -0600

    feat: add codediff for vim diffs and mergetool

---
+++ config/nvim/lua/w/plugins/codediff.lua
@@ -0,0 +1,10 @@
+--
+-- codediff.nvim
+-- https://github.com/esmuellert/codediff.nvim
+--
+
+return {
+  'esmuellert/codediff.nvim',
+  dependencies = { 'MunifTanjim/nui.nvim' },
+  cmd = 'CodeDiff',
+}
