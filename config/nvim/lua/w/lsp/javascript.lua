d8bb8b5d61e16d3b695198f65d609f4a43b68b57
Wassim Metallaoui <wassim@metallaoui.com>
2026-02-15 09:13:06 -0600

    refactor: ai assisted code cleanup

---
+++ config/nvim/lua/w/lsp/javascript.lua
@@ -0,0 +1,29 @@
+--
+-- javascript/typescript shared formatting
+--
+
+local M = {}
+
+function M.setup_formatting()
+  local auGroup = vim.api.nvim_create_augroup('WamAutocmdsJavaScriptFormatting', {})
+
+  vim.api.nvim_create_autocmd('BufWritePre', {
+    callback = function()
+      vim.lsp.buf.format()
+      require('conform').format()
+
+      -- https://github.com/pmizio/typescript-tools.nvim/blob/master/lua/typescript-tools/user_commands.lua
+      -- require('typescript-tools.api').sort_imports(true)
+      -- require('typescript-tools.api').organize_imports(true)
+      -- require('typescript-tools.api').remove_unused(true)
+      -- require('typescript-tools.api').remove_unused_imports(true)
+      -- require('typescript-tools.api').add_missing_imports(true)
+      require('typescript-tools.api').fix_all(true)
+
+      vim.cmd('silent! EslintFixAll')
+    end,
+    group = auGroup,
+  })
+end
+
+return M
