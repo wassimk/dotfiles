4d9dd21324fc6d956c72bf77e27028439471feb0
Wassim Metallaoui <wassim@metallaoui.com>
2026-02-22 19:02:12 -0600

    feat: add messages to buffer command (why this so hard???)

--- config/nvim/lua/w/keymaps.lua
+++ config/nvim/lua/w/keymaps.lua
@@ -9,6 +9,9 @@ vim.keymap.set({ 'n', 'v' }, '<Leader>y', '"+y', { desc = 'copy to system clipbo
 vim.keymap.set('n', '<leader>q', require('w.utils').toggle_qf, { desc = 'toggle quickfix' })
 vim.keymap.set('n', '<leader>l', require('w.utils').toggle_loclist, { desc = 'toggle loclist' })
 
+-- messages
+vim.keymap.set('n', '<leader>m', '<cmd>MessagesToBuffer<cr>', { desc = 'messages to buffer' })
+
 -- execute current line or file
 vim.keymap.set('n', '<leader>x', '<cmd>.lua<CR>', { desc = 'execute current line' })
 vim.keymap.set('n', '<leader><leader>x', '<cmd>source %<CR>', { desc = 'execute current file' })
--- config/nvim/lua/w/usercmds.lua
+++ config/nvim/lua/w/usercmds.lua
@@ -9,3 +9,13 @@ end, { desc = 'lazygit: launch with toggleterm' })
 vim.api.nvim_create_user_command('LspFormat', function()
   vim.lsp.buf.format()
 end, { desc = 'lsp: format with language servers' })
+
+vim.api.nvim_create_user_command('MessagesToBuffer', function()
+  local buf = vim.api.nvim_create_buf(false, true)
+  local messages = vim.split(vim.api.nvim_exec2('messages', { output = true }).output, '\n')
+  vim.api.nvim_buf_set_lines(buf, 0, -1, false, messages)
+  vim.bo[buf].modifiable = false
+  vim.bo[buf].bufhidden = 'wipe'
+  vim.api.nvim_open_win(buf, true, { split = 'below' })
+  vim.cmd('normal! G')
+end, { desc = 'Open :messages in a scratch buffer' })
