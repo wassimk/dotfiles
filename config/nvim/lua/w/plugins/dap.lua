ed0ba2ad9999873e5cbafdd54412d069e9cece50
Wassim Metallaoui <wassim@metallaoui.com>
2026-09-06 11:11:53 -0500

    refactor: move ruby dap configurations out of the ftplugin
    The ruby ftplugin required dap at load time, so the debugger
    configurations now live in the dap plugin config alongside the rest of
    the dap setup. Ruby files still load nvim-dap because ruby-lsp.nvim
    requires it to register its adapter, so this is a cleanup rather than a
    lazy-loading win.

--- config/nvim/lua/w/plugins/dap.lua
+++ config/nvim/lua/w/plugins/dap.lua
@@ -80,6 +80,70 @@ return {
     local dap = require('dap')
     local widgets = require('dap.ui.widgets')
 
+    -- ruby (adapter registered by ruby-lsp.nvim)
+    dap.configurations.ruby = {
+      {
+        type = 'ruby',
+        name = 'debug file',
+        request = 'attach',
+        localfs = true,
+        command = { 'ruby' },
+        script = { '${relativeFile}' },
+      },
+      {
+        type = 'ruby',
+        name = 'debug file (prompt)',
+        request = 'attach',
+        localfs = true,
+        command = { 'ruby' },
+        script = { '${relativeFile}' },
+        script_args = function()
+          local argument_string = vim.fn.input('Program arguments: ')
+          return vim.fn.split(argument_string, ' ', true)
+        end,
+      },
+      {
+        type = 'ruby',
+        name = 'debug test file',
+        request = 'attach',
+        localfs = true,
+        command = { 'ruby', 'bin/rails', 'test' },
+        script = { '${relativeFile}' },
+      },
+      {
+        type = 'ruby',
+        name = 'debug test nearest',
+        request = 'attach',
+        localfs = true,
+        command = { 'ruby', 'bin/rails', 'test' },
+        script = function()
+          local line = vim.api.nvim_win_get_cursor(0)[1]
+
+          return { '${relativeFile}:' .. line }
+        end,
+      },
+      {
+        type = 'ruby',
+        name = 'debug spec file',
+        request = 'attach',
+        localfs = true,
+        command = { 'ruby', 'bin/rspec' },
+        script = { '${relativeFile}' },
+      },
+      {
+        type = 'ruby',
+        name = 'debug spec nearest',
+        request = 'attach',
+        localfs = true,
+        command = { 'ruby', 'bin/rspec' },
+        script = function()
+          local line = vim.api.nvim_win_get_cursor(0)[1]
+
+          return { '${relativeFile}:' .. line }
+        end,
+      },
+    }
+
     vim.keymap.set('n', '<Leader>dR', dap.repl.open, opts('REPL open'))
     vim.keymap.set('n', '<Leader>dl', dap.run_last, opts('run last'))
     vim.keymap.set('n', '<Leader>dr', dap.run_to_cursor, opts('debug at cursor'))
